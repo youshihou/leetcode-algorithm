d14679fac69d1cc6d4129b4cf28f67e801f9c60a
wangak <youshihou5@gmail.com>
2020-04-20 23:23:36 +0800

    t16 sub sort lcci

--- algorithm/algorithm/array-sort/T16-sub-sort-lcci.c
+++ algorithm/algorithm/array-sort/T16-sub-sort-lcci.c
@@ -7,11 +7,50 @@
 //
 
 #include "T16-sub-sort-lcci.h"
-
+#include <stdlib.h>
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* subSort(int* array, int arraySize, int* returnSize) {
+    int* result = (int *)malloc(sizeof(int) * 2);
+    result[0] = result[1] = -1;
+    *returnSize = 2;
+
+    if (array == NULL || arraySize == 0) {
+        return result;
+    }
     
-    return NULL;
+    // from left to right, find min
+    int max = array[0];
+    int r = -1;
+    for (int i = 1; i < arraySize; i++) {
+        int v = array[i];
+        if (v < max) {
+            r = i;
+        } else {
+            max = v;
+        }
+    }
+    
+    // no need sort
+    if (r == -1) {
+        return result;
+    }
+    
+    // from right to left, find max
+    int min = array[arraySize - 1];
+    int l = - 1;
+    for (int i = arraySize - 2; i >= 0; i--) {
+        int v = array[i];
+        if (v > min) {
+            l = i;
+        } else {
+            min = v;
+        }
+    }
+
+    result[0] = l;
+    result[1] = r;
+    
+    return result;
 }
--- algorithm/algorithm/main.c
+++ algorithm/algorithm/main.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include "T88-merge-sorted-array.h"
+#include "T16-sub-sort-lcci.h"
 
 int main(int argc, const char * argv[]) {
     
@@ -15,6 +16,14 @@ int main(int argc, const char * argv[]) {
         int nums1[1] = {0};
         int nums2[1] = {1};
         merge(nums1, 1, 0, nums2, 1, 1);
+    }
+    
+    {
+        // T16
+        int array[] = {};
+        subSort(array, 0, NULL);
+        int array2[] = {1,2,4,7,10,11,7,12,6,7,16,18,19};
+        subSort(array2, 13, NULL);
     }
 
     return 0;
