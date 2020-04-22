f076ba3f123672f28f2a06029b6c2b60a721e66a
wangak <youshihou5@gmail.com>
2020-04-22 22:07:05 +0800

    t2 add-two-numbers

---
+++ algorithm/algorithm/linked-list/T2-add-two-numbers.c
@@ -0,0 +1,63 @@
+//
+//  T2-add-two-numbers.c
+//  algorithm
+//
+//  Created by Ankui on 4/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T2-add-two-numbers.h"
+#include "linked-list-common.h"
+#include <stdlib.h>
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    if (l1 == NULL) { return l2; }
+    if (l2 == NULL) { return l1; }
+    
+    struct ListNode* virtualHead = malloc(sizeof(struct ListNode));
+    virtualHead->next = NULL; // MUST!!!
+    struct ListNode* last = virtualHead;
+    
+    int carry = 0;
+    while (l1 != NULL || l2 != NULL) {
+        int v1 = 0;
+        if (l1 != NULL) {
+            v1 = l1->val;
+            l1 = l1->next;
+        }
+        int v2 = 0;
+        if (l2 != NULL) {
+            v2 = l2->val;
+            l2 = l2->next;
+        }
+        
+        int sum = v1 + v2 + carry;
+        
+        struct ListNode* node = malloc(sizeof(struct ListNode));
+        node->val = sum % 10;
+        node->next = NULL; // MUST!!!
+        carry = sum / 10;
+        last->next = node;
+        last = node;
+    }
+    
+    if (carry) {
+        struct ListNode *node = malloc(sizeof(struct ListNode));
+        node->val = carry;
+        node->next = NULL; // MUST!!!
+        last->next = node;
+        last = node;
+    }
+    
+    return virtualHead->next;
+}
---
+++ algorithm/algorithm/linked-list/T2-add-two-numbers.h
@@ -0,0 +1,18 @@
+//
+//  T2-add-two-numbers.h
+//  algorithm
+//
+//  Created by Ankui on 4/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T2_add_two_numbers_h
+#define T2_add_two_numbers_h
+
+// https://leetcode-cn.com/problems/add-two-numbers/
+
+#include <stdio.h>
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2);
+
+#endif /* T2_add_two_numbers_h */
