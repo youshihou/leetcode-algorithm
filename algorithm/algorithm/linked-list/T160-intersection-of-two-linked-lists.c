ea011030c2c896d3b5c8c0a159b5a36109d98ee2
oneday <youshihou5@gmail.com>
2020-04-22 23:03:47 +0800

    t160-intersection-of-two-linked-lists

---
+++ algorithm/algorithm/linked-list/T160-intersection-of-two-linked-lists.c
@@ -0,0 +1,36 @@
+//
+//  T160-intersection-of-two-linked-lists.c
+//  algorithm
+//
+//  Created by Ankui on 4/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T160-intersection-of-two-linked-lists.h"
+#include "linked-list-common.h"
+#include <stdlib.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if (headA == NULL || headB == NULL) { return NULL; }
+    
+    struct ListNode* curA = headA;
+    struct ListNode* curB = headB;
+    
+    while (curA != curB) { 
+        curA = curA == NULL ? headB : curA->next;
+        curB = curB == NULL ? headA : curB->next;
+        
+        // when no intersection, then curA->next = NULL && curB->next == NULL, could not exit loop!!!
+//        curA = curA->next == NULL ? headB : curA->next;
+//        curB = curB->next == NULL ? headA : curB->next;
+    }
+
+    return curA;
+}
---
+++ algorithm/algorithm/linked-list/T160-intersection-of-two-linked-lists.h
@@ -0,0 +1,18 @@
+//
+//  T160-intersection-of-two-linked-lists.h
+//  algorithm
+//
+//  Created by Ankui on 4/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T160_intersection_of_two_linked_lists_h
+#define T160_intersection_of_two_linked_lists_h
+
+// https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+
+#include <stdio.h>
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB);
+
+#endif /* T160_intersection_of_two_linked_lists_h */
