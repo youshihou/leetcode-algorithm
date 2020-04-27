439961b24a3f098e4b615020ce5dec6d41533445
oneday <youshihou5@gmail.com>
2020-04-27 23:20:48 +0800

    add T242-valid-anagram task

---
+++ algorithm/algorithm/string/T242-valid-anagram.c
@@ -0,0 +1,9 @@
+//
+//  T242-valid-anagram.c
+//  algorithm
+//
+//  Created by Ankui on 4/27/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T242-valid-anagram.h"
---
+++ algorithm/algorithm/string/T242-valid-anagram.h
@@ -0,0 +1,14 @@
+//
+//  T242-valid-anagram.h
+//  algorithm
+//
+//  Created by Ankui on 4/27/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T242_valid_anagram_h
+#define T242_valid_anagram_h
+
+#include <stdio.h>
+
+#endif /* T242_valid_anagram_h */
