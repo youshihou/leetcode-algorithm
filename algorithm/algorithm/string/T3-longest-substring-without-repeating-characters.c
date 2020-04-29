af3b47acc0e1f0d4e45b8326c44b86ece5d8a507
oneday <youshihou5@gmail.com>
2020-04-29 22:10:12 +0800

    add T3-longest-substring-without-repeating-characters task

---
+++ algorithm/algorithm/string/T3-longest-substring-without-repeating-characters.c
@@ -0,0 +1,16 @@
+//
+//  T3-longest-substring-without-repeating-characters.c
+//  algorithm
+//
+//  Created by Ankui on 4/29/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+// https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+
+#include "T3-longest-substring-without-repeating-characters.h"
+
+int lengthOfLongestSubstring(char * s) {
+
+    return 0;
+}
---
+++ algorithm/algorithm/string/T3-longest-substring-without-repeating-characters.h
@@ -0,0 +1,14 @@
+//
+//  T3-longest-substring-without-repeating-characters.h
+//  algorithm
+//
+//  Created by Ankui on 4/29/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T3_longest_substring_without_repeating_characters_h
+#define T3_longest_substring_without_repeating_characters_h
+
+#include <stdio.h>
+
+#endif /* T3_longest_substring_without_repeating_characters_h */
