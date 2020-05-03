43d3e7d6de2a30ede08fccbdf2b1b6c8810b487e
oneday <youshihou5@gmail.com>
2020-05-03 18:13:13 +0800

    T22-generate-parentheses task

---
+++ algorithm/algorithm/depth-first-search/T22-generate-parentheses.c
@@ -0,0 +1,9 @@
+//
+//  T22-generate-parentheses.c
+//  algorithm
+//
+//  Created by Ankui on 5/3/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T22-generate-parentheses.h"
---
+++ algorithm/algorithm/depth-first-search/T22-generate-parentheses.h
@@ -0,0 +1,14 @@
+//
+//  T22-generate-parentheses.h
+//  algorithm
+//
+//  Created by Ankui on 5/3/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T22_generate_parentheses_h
+#define T22_generate_parentheses_h
+
+#include <stdio.h>
+
+#endif /* T22_generate_parentheses_h */
