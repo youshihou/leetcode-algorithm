3f06dac7c0d2570b79cbeec2e97ba6612003b74c
oneday <youshihou5@gmail.com>
2020-05-03 15:35:49 +0800

    T46-permutations task

---
+++ algorithm/algorithm/depth-first-search/T46-permutations.h
@@ -0,0 +1,14 @@
+//
+//  T46-permutations.h
+//  algorithm
+//
+//  Created by Ankui on 5/3/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T46_permutations_h
+#define T46_permutations_h
+
+#include <stdio.h>
+
+#endif /* T46_permutations_h */
