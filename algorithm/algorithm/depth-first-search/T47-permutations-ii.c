1fb5fd851e3cc88b1f719f042f5fa7bff9e9aa6a
oneday <youshihou5@gmail.com>
2020-05-03 17:04:31 +0800

    T47-permutations-ii task

---
+++ algorithm/algorithm/depth-first-search/T47-permutations-ii.c
@@ -0,0 +1,9 @@
+//
+//  T47-permutations-ii.c
+//  algorithm
+//
+//  Created by Ankui on 5/3/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T47-permutations-ii.h"
---
+++ algorithm/algorithm/depth-first-search/T47-permutations-ii.h
@@ -0,0 +1,14 @@
+//
+//  T47-permutations-ii.h
+//  algorithm
+//
+//  Created by Ankui on 5/3/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T47_permutations_ii_h
+#define T47_permutations_ii_h
+
+#include <stdio.h>
+
+#endif /* T47_permutations_ii_h */
