3829ee0a1368104d75f325c7a745dc16ca2b36ce
oneday <youshihou5@gmail.com>
2020-05-04 23:33:55 +0800

    add T50-powx-n task

---
+++ algorithm/algorithm/high-frequency/T62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 5/4/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof.h"
---
+++ algorithm/algorithm/high-frequency/T62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 5/4/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T62_yuan_quan_zhong_zui_hou_sheng_xia_de_shu_zi_lcof_h
+#define T62_yuan_quan_zhong_zui_hou_sheng_xia_de_shu_zi_lcof_h
+
+#include <stdio.h>
+
+#endif /* T62_yuan_quan_zhong_zui_hou_sheng_xia_de_shu_zi_lcof_h */
