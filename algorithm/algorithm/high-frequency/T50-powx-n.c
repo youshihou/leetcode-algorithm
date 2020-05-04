77efd1528d5910b7515ca6c3764d15827ebc35a1
oneday <youshihou5@gmail.com>
2020-05-04 23:34:00 +0800

    add T50-powx-n task

--- algorithm/algorithm/high-frequency/T50-powx-n.c
+++ algorithm/algorithm/high-frequency/T50-powx-n.c
@@ -31,9 +31,8 @@ double myPow1(double x, int n) {
     if (n == 0) { return 1; }
     if (n == -1) { return 1/x; } // CARE!!!
     
-    bool odd = (n & 1) == 1;
     double half = myPow(x, n >> 1);
     half *= half;
 
-    return odd ? half * x : half;
+    return ((n & 1) == 1) ? half * x : half;
 }
--- algorithm/algorithm/high-frequency/T62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof.c
+++ algorithm/algorithm/high-frequency/T62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof.c
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
+
 #include "T62-yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof.h"
+
+int lastRemaining(int n, int m) {
+    if (n <= 0 || m <= 0) { return 0; }
+    
+    // f(1, 3) = 0;
+    // f(2, 3) = (f(1, 3) + 3) % 2;
+    // f(8, 3) = (f(7, 3) + 3) % 6;
+    // f(9, 3) = (f(8, 3) + 3) % 9;
+    // f(10, 3) = (f(9, 3) + 3) % 10;
+    int result = 0;
+    for (int i = 2; i <= n; i++) { // CARE!!! i = 2, maybe i = 1
+        result = (result + m) % i;
+    }
+    
+    return result;}
+
+
+int lastRemaining1(int n, int m) {
+    if (n <= 0 || m <= 0) { return 0; }
+
+    // f(n, m) = (f(n - 1, m) + m) % n;
+    if (n == 1) { return 0; }
+    return (lastRemaining(n - 1, m) + m) % n;
+}
