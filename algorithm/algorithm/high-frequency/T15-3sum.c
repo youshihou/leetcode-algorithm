c46d09723c435e98aaee38b5b627fce82b7ed3ac
oneday <youshihou5@gmail.com>
2020-05-04 18:40:10 +0800

    add T15-3sum task

---
+++ algorithm/algorithm/high-frequency/T15-3sum.c
@@ -0,0 +1,9 @@
+//
+//  T15-3sum.c
+//  algorithm
+//
+//  Created by Ankui on 5/4/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T15-3sum.h"
---
+++ algorithm/algorithm/high-frequency/T15-3sum.h
@@ -0,0 +1,14 @@
+//
+//  T15-3sum.h
+//  algorithm
+//
+//  Created by Ankui on 5/4/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T15_3sum_h
+#define T15_3sum_h
+
+#include <stdio.h>
+
+#endif /* T15_3sum_h */
