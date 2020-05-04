02312432470cce3e58c00af47e8db067b6566bda
oneday <youshihou5@gmail.com>
2020-05-04 12:45:38 +0800

    T93-restore-ip-addresses task

--- algorithm/algorithm/depth-first-search/T93-restore-ip-addresses.c
+++ algorithm/algorithm/depth-first-search/T93-restore-ip-addresses.c
@@ -26,45 +26,57 @@ bool isValid(char* s, int len) {
     return num > 255 ? false : true;
 }
 
-void dfs(char* s, int* returnSize, char* string, char** list, int idx, int start) {
-    if (start > 4) { return; }
-    if (s[0] == '\0' && start == 4) {
-        list[*returnSize] = malloc(sizeof(char) * (strlen(string) + 1));
-        strcpy(list[*returnSize], string);
-        (*returnSize)++;
-        return;
-    }
-    
-    if (start > 0 && start < 4) {
-        string[idx++] = '.';
-    }
-    for (int i = 1; i <= 3 && i <= strlen(s); i++) {
-        if (isValid(s, i)) {
-            for (int j = 0; j < i; j++) {
-                string[idx + j] = s[j];
-            }
-            dfs(s + i, returnSize, string, list, idx + i, start + 1);
-        }
-    }
-}
+// TODO: - 
+//void dfs(char* s, int* returnSize, char** list, int num, int* points) {
+//    int len = (int)strlen(s);
+//    if (num == 3) {
+//        if (isValid(s + points[2], len - points[2])) {
+//            char* string = malloc(sizeof(char) * (len + 4));
+//            memset(string, '\0', sizeof(char) * (len + 4));
+//            int k = 0;
+//            for (int i = 0; i < len; i++) {
+//                for (int j = 0; j < 3; j++) {
+//                    if (i == points[j]) {
+//                        string[k++] = '.';
+//                    }
+//                }
+//                string[k++] = s[i];
+//            }
+//            list[*returnSize] = string;
+//            (*returnSize)++;
+//        }
+//        return;
+//    }
+//
+//    int pos = num == 0 ? 0 : num - 1;
+//    for (int i = pos + 1; i < len; i++) {
+//        if (isValid(s + pos, i - pos)) {
+//            points[num] = i;
+//            num++;
+//            dfs(s, returnSize, list, num, points);
+//            num--;
+//        }
+//    }
+//}
+//
+//char ** restoreIpAddresses(char * s, int* returnSize) {
+//    *returnSize = 0;
+//    if (s == NULL) { return NULL; }
+//    size_t len = strlen(s);
+//    if (len < 4 || len > 12) { return NULL; }
+//
+//    int maxLen = 1024;
+//    char** list = malloc(sizeof(char*) * maxLen);
+//    memset(list, '\0', sizeof(char*) * maxLen);
+//
+//    int* points = malloc(sizeof(int) * 3);
+//    memset(points, 0, sizeof(int) * 3);
+//
+//    dfs(s, returnSize, list, 0, points);
+//
+//    return list;
+//}
 
-char ** restoreIpAddresses(char * s, int* returnSize) {
-    *returnSize = 0;
-    if (s == NULL) { return NULL; }
-    size_t len = strlen(s);
-    if (len < 4 || len > 12) { return NULL; }
-    
-    int maxLen = 1024;
-    char** list = malloc(sizeof(char*) * maxLen);
-    memset(list, '\0', sizeof(char*) * maxLen);
-    
-    char* string = malloc(sizeof(char) * (len + 4));
-    memset(string, '\0', sizeof(char) * (len + 4));
-
-    dfs(s, returnSize, string, list, 0, 0);
-    
-    return list;
-}
 
 
 
@@ -105,7 +117,7 @@ char ** restoreIpAddresses1(char * s, int* returnSize) {
     char* string = malloc(sizeof(char) * (len + 4));
     memset(string, '\0', sizeof(char) * (len + 4));
 
-    dfs(s, returnSize, string, list, 0, 0);
+    dfs1111(s, returnSize, string, list, 0, 0);
     
     return list;
 }
