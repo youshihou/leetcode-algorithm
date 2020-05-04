a4a9c39a6eb2409c6df3ce605045f34885b41dd9
oneday <youshihou5@gmail.com>
2020-05-04 20:18:25 +0800

    add T50-powx-n task

--- algorithm/algorithm/high-frequency/T50-powx-n.c
+++ algorithm/algorithm/high-frequency/T50-powx-n.c
@@ -12,6 +12,22 @@
 #include "high-frequency-common.h"
 
 double myPow(double x, int n) {
+    double result = 1.0;
+//    int y = n < 0 ? -n : n; //CARE!!! when n = -2^31, -n overflow
+    long y = n < 0 ? -((long)n) : n;
+    while (y > 0) {
+        if ((y & 1) == 1) {
+            result *= x;
+        }
+        x *= x;
+         
+        y >>= 1;
+    }
+    
+    return n < 0 ? (1/result) : result;
+}
+
+double myPow1(double x, int n) {
     if (n == 0) { return 1; }
     if (n == -1) { return 1/x; } // CARE!!!
     
