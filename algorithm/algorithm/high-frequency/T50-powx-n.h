b94ea41c6de09b9dc354cf242a4e94ded25acee9
oneday <youshihou5@gmail.com>
2020-05-04 19:19:42 +0800

    add T50-powx-n task

---
+++ algorithm/algorithm/high-frequency/T50-powx-n.h
@@ -0,0 +1,14 @@
+//
+//  T50-powx-n.h
+//  algorithm
+//
+//  Created by Ankui on 5/4/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T50_powx_n_h
+#define T50_powx_n_h
+
+#include <stdio.h>
+
+#endif /* T50_powx_n_h */
