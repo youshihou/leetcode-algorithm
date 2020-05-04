63005d952b59ff153aa1a50e54b22ec82efe8aef
oneday <youshihou5@gmail.com>
2020-05-04 19:19:47 +0800

    add T50-powx-n task

--- algorithm/algorithm/high-frequency/T50-powx-n.c
+++ algorithm/algorithm/high-frequency/T50-powx-n.c
@@ -6,4 +6,18 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/powx-n/
+
 #include "T50-powx-n.h"
+#include "high-frequency-common.h"
+
+double myPow(double x, int n) {
+    if (n == 0) { return 1; }
+    if (n == -1) { return 1/x; } // CARE!!!
+    
+    bool odd = (n & 1) == 1;
+    double half = myPow(x, n >> 1);
+    half *= half;
+
+    return odd ? half * x : half;
+}
