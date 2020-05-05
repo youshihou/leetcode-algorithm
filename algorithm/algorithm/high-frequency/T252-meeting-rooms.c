2adcbba40282916d5ff15360436c64290751c860
oneday <youshihou5@gmail.com>
2020-05-05 20:01:40 +0800

    add T252-meeting-rooms task

---
+++ algorithm/algorithm/high-frequency/T252-meeting-rooms.c
@@ -0,0 +1,9 @@
+//
+//  T252-meeting-rooms.c
+//  algorithm
+//
+//  Created by Ankui on 5/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T252-meeting-rooms.h"
---
+++ algorithm/algorithm/high-frequency/T252-meeting-rooms.h
@@ -0,0 +1,14 @@
+//
+//  T252-meeting-rooms.h
+//  algorithm
+//
+//  Created by Ankui on 5/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T252_meeting_rooms_h
+#define T252_meeting_rooms_h
+
+#include <stdio.h>
+
+#endif /* T252_meeting_rooms_h */
