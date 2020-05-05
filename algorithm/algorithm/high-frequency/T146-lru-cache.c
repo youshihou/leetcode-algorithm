a54d53f088ae23fd88ecf009318b89cb496451c0
oneday <youshihou5@gmail.com>
2020-05-05 13:58:23 +0800

    add T146-lru-cache task

---
+++ algorithm/algorithm/high-frequency/T146-lru-cache.c
@@ -0,0 +1,9 @@
+//
+//  T146-lru-cache.c
+//  algorithm
+//
+//  Created by Ankui on 5/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T146-lru-cache.h"
---
+++ algorithm/algorithm/high-frequency/T146-lru-cache.h
@@ -0,0 +1,14 @@
+//
+//  T146-lru-cache.h
+//  algorithm
+//
+//  Created by Ankui on 5/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T146_lru_cache_h
+#define T146_lru_cache_h
+
+#include <stdio.h>
+
+#endif /* T146_lru_cache_h */
