ec23516f9fb0eefc420e53021ff5ff81d7071014
oneday <youshihou5@gmail.com>
2020-05-05 20:01:45 +0800

    add T252-meeting-rooms task

--- algorithm/algorithm/high-frequency/T252-meeting-rooms.c
+++ algorithm/algorithm/high-frequency/T252-meeting-rooms.c
@@ -6,4 +6,31 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/meeting-rooms/
+
 #include "T252-meeting-rooms.h"
+#include "high-frequency-common.h"
+
+
+int compare(const void* a, const void* b) {
+    int* aa = *(int**)a;
+    int* bb = *(int**)b;
+    
+    return *aa - *bb;
+    return *(*(int**)a) - *(*(int**)b);
+}
+
+bool canAttendMeetings(int** intervals, int intervalsSize, int* intervalsColSize) {
+//    *intervalsColSize = 0; // ERROR!!!
+    if (intervals == NULL || intervalsSize <= 1) { return true; }
+
+    qsort(intervals, intervalsSize, sizeof(intervals[0]), compare);
+    
+    for (int i = 1; i < intervalsSize; i++) {
+        if (intervals[i][0] < intervals[i - 1][1]) { // CARE!!! [1]
+            return false;
+        }
+    }
+    
+    return true;
+}
