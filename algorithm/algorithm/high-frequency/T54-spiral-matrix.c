92963bdb3e55a52d742cd228903e540f1441f0ad
oneday <youshihou5@gmail.com>
2020-05-05 10:37:40 +0800

    add T54-spiral-matrix task

--- algorithm/algorithm/high-frequency/T54-spiral-matrix.c
+++ algorithm/algorithm/high-frequency/T54-spiral-matrix.c
@@ -6,4 +6,60 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/spiral-matrix/
+
 #include "T54-spiral-matrix.h"
+#include "high-frequency-common.h"
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
+    *returnSize = 0;
+    if (matrix == NULL || matrixSize <= 0 || *matrixColSize <= 0) { return NULL; }
+    
+    int* list = malloc(sizeof(int) * (matrixSize * (*matrixColSize)));
+
+    int top = 0;
+    int right = *matrixColSize - 1;
+    int left = 0;
+    int bottom = matrixSize - 1;
+    
+    while (top <= bottom && left <= right) {
+        // left top  -> right top
+        for (int i = left; i <= right; i++) {
+            list[*returnSize] = matrix[top][i];
+            (*returnSize)++;
+        }
+        top++;
+//        if (top > bottom) break; // CARE!!!
+
+        // right top -> right bottom
+        for (int i = top; i <= bottom; i++) {
+            list[*returnSize] = matrix[i][right];
+            (*returnSize)++;
+        }
+        right--;
+//        if (left > right) break; // CARE!!!
+        
+        if (top > bottom || left > right) {
+            break;
+        }
+         
+        // right bottom -> left bottom
+        for (int i = right; i >= left; i--) {
+            list[*returnSize] = matrix[bottom][i];
+            (*returnSize)++;
+        }
+        bottom--;
+        // left bottom -> left top
+        for (int i = bottom; i >= top; i--) {
+            list[*returnSize] = matrix[i][left];
+            (*returnSize)++;
+        }
+        left++;
+    }
+        
+    return list;
+
+}
