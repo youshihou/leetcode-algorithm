ca50c58f4d633386f75c0fa76024330770babec6
oneday <youshihou5@gmail.com>
2020-05-05 19:34:25 +0800

    add T7-reverse-integer task

--- algorithm/algorithm/high-frequency/T7-reverse-integer.c
+++ algorithm/algorithm/high-frequency/T7-reverse-integer.c
@@ -6,4 +6,36 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/reverse-integer/
+
 #include "T7-reverse-integer.h"
+#include <limits.h>
+
+//int reverse(int x) {
+//    int result = 0;
+//    while (x != 0) {
+//        int prev = result;
+//        int mod = x % 10;
+//        result = prev * 10 + mod;
+//        if ((result - mod) / 10 != prev) {
+//            return 0;
+//        }
+//        x /= 10;
+//    }
+//
+//    return result;
+//}
+
+
+int reverse(int x) {
+    long result = 0;
+    while (x != 0) {
+        result = result * 10 + x % 10;
+        if (result > INT_MAX || result < INT_MIN) {
+            return 0;
+        }
+        x /= 10;
+    }
+    
+    return (int)result;
+}
