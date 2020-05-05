50f48ed27a3b15bedec56f23a25c178611cf7ad4
oneday <youshihou5@gmail.com>
2020-05-05 19:34:20 +0800

    add T7-reverse-integer task

---
+++ algorithm/algorithm/high-frequency/T7-reverse-integer.c
@@ -0,0 +1,9 @@
+//
+//  T7-reverse-integer.c
+//  algorithm
+//
+//  Created by Ankui on 5/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T7-reverse-integer.h"
---
+++ algorithm/algorithm/high-frequency/T7-reverse-integer.h
@@ -0,0 +1,14 @@
+//
+//  T7-reverse-integer.h
+//  algorithm
+//
+//  Created by Ankui on 5/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T7_reverse_integer_h
+#define T7_reverse_integer_h
+
+#include <stdio.h>
+
+#endif /* T7_reverse_integer_h */
