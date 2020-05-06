9332c061ec7117d1fc74307b8ebc756de61f7fee
oneday <youshihou5@gmail.com>
2020-05-06 21:39:33 +0800

    T42-trapping-rain-water

---
+++ algorithm/algorithm/high-frequency/T42-trapping-rain-water.c
@@ -0,0 +1,9 @@
+//
+//  T42-trapping-rain-water.c
+//  algorithm
+//
+//  Created by Ankui on 5/6/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T42-trapping-rain-water.h"
---
+++ algorithm/algorithm/high-frequency/T42-trapping-rain-water.h
@@ -0,0 +1,14 @@
+//
+//  T42-trapping-rain-water.h
+//  algorithm
+//
+//  Created by Ankui on 5/6/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T42_trapping_rain_water_h
+#define T42_trapping_rain_water_h
+
+#include <stdio.h>
+
+#endif /* T42_trapping_rain_water_h */
