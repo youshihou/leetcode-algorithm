98f92495447e76207c5022959f70b17aa52890ab
oneday <youshihou5@gmail.com>
2020-05-07 21:58:00 +0800

    add T215-kth-largest-element-in-an-array

--- algorithm/algorithm/high-frequency/T215-kth-largest-element-in-an-array.c
+++ algorithm/algorithm/high-frequency/T215-kth-largest-element-in-an-array.c
@@ -9,8 +9,17 @@
 // https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
 
 #include "T215-kth-largest-element-in-an-array.h"
+#include "high-frequency-common.h"
+
+int t215compare(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
 
 int findKthLargest(int* nums, int numsSize, int k) {
-
-    return 0;
+    if (nums == NULL || numsSize <= 0) { return 0; }
+    
+    qsort(nums, numsSize, sizeof(int), t215compare);
+    
+    return nums[numsSize - k];
 }
--- algorithm/algorithm/high-frequency/T315-count-of-smaller-numbers-after-self.c
+++ algorithm/algorithm/high-frequency/T315-count-of-smaller-numbers-after-self.c
@@ -9,11 +9,19 @@
 // https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/
 
 #include "T315-count-of-smaller-numbers-after-self.h"
+#include "high-frequency-common.h"
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* countSmaller(int* nums, int numsSize, int* returnSize) {
-
-    return NULL;
+    *returnSize = 0;
+    if (nums == NULL || numsSize == 0) { return NULL; }
+    
+    int *list = malloc(sizeof(int) * numsSize);
+    memset(list, 0, sizeof(int) * numsSize);
+    
+    
+    
+    return list;
 }
