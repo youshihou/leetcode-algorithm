c238f3e14a173e24763cbe366b6d0b3b72178e39
oneday <youshihou5@gmail.com>
2020-05-10 23:57:33 +0800

    add T150-evaluate-reverse-polish-notation

---
+++ algorithm/algorithm/stack-queue/T150-evaluate-reverse-polish-notation.c
@@ -0,0 +1,9 @@
+//
+//  T150-evaluate-reverse-polish-notation.c
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T150-evaluate-reverse-polish-notation.h"
---
+++ algorithm/algorithm/stack-queue/T150-evaluate-reverse-polish-notation.h
@@ -0,0 +1,14 @@
+//
+//  T150-evaluate-reverse-polish-notation.h
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T150_evaluate_reverse_polish_notation_h
+#define T150_evaluate_reverse_polish_notation_h
+
+#include <stdio.h>
+
+#endif /* T150_evaluate_reverse_polish_notation_h */
