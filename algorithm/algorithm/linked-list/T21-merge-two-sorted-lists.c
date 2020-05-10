56582a8bf6437d9a6da5616331149944dedac895
oneday <youshihou5@gmail.com>
2020-05-10 17:46:55 +0800

    add T21-merge-two-sorted-lists

---
+++ algorithm/algorithm/linked-list/T21-merge-two-sorted-lists.c
@@ -0,0 +1,9 @@
+//
+//  T21-merge-two-sorted-lists.c
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T21-merge-two-sorted-lists.h"
---
+++ algorithm/algorithm/linked-list/T21-merge-two-sorted-lists.h
@@ -0,0 +1,14 @@
+//
+//  T21-merge-two-sorted-lists.h
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T21_merge_two_sorted_lists_h
+#define T21_merge_two_sorted_lists_h
+
+#include <stdio.h>
+
+#endif /* T21_merge_two_sorted_lists_h */
