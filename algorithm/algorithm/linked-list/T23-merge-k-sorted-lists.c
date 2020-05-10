e81ba97d4810ca9e3cf8de5a1d61a022ba1e9fe7
oneday <youshihou5@gmail.com>
2020-05-10 19:36:28 +0800

    add T23-merge-k-sorted-lists

---
+++ algorithm/algorithm/linked-list/T23-merge-k-sorted-lists.c
@@ -0,0 +1,9 @@
+//
+//  T23-merge-k-sorted-lists.c
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T23-merge-k-sorted-lists.h"
---
+++ algorithm/algorithm/linked-list/T23-merge-k-sorted-lists.h
@@ -0,0 +1,14 @@
+//
+//  T23-merge-k-sorted-lists.h
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T23_merge_k_sorted_lists_h
+#define T23_merge_k_sorted_lists_h
+
+#include <stdio.h>
+
+#endif /* T23_merge_k_sorted_lists_h */
