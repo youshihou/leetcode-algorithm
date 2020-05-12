f21ec4dc75eef39472f9685e18d5f71e3f4bac3c
oneday <youshihou5@gmail.com>
2020-05-12 22:16:03 +0800

    add T124-binary-tree-maximum-path-sum

--- algorithm/algorithm/binary-tree/T124-binary-tree-maximum-path-sum.c
+++ algorithm/algorithm/binary-tree/T124-binary-tree-maximum-path-sum.c
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/
+
 #include "T124-binary-tree-maximum-path-sum.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int maxPathSum(struct TreeNode* root) {
+
+    return 0;
+}
