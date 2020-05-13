bb1ee66d5301df16116f01430b173e045885e371
oneday <youshihou5@gmail.com>
2020-05-13 21:32:42 +0800

    T14-flatten-binary-tree-to-linked-list task

---
+++ algorithm/algorithm/binary-tree/T14-flatten-binary-tree-to-linked-list.c
@@ -0,0 +1,9 @@
+//
+//  T14-flatten-binary-tree-to-linked-list.c
+//  algorithm
+//
+//  Created by Ankui on 5/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T14-flatten-binary-tree-to-linked-list.h"
---
+++ algorithm/algorithm/binary-tree/T14-flatten-binary-tree-to-linked-list.h
@@ -0,0 +1,14 @@
+//
+//  T14-flatten-binary-tree-to-linked-list.h
+//  algorithm
+//
+//  Created by Ankui on 5/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T14_flatten_binary_tree_to_linked_list_h
+#define T14_flatten_binary_tree_to_linked_list_h
+
+#include <stdio.h>
+
+#endif /* T14_flatten_binary_tree_to_linked_list_h */
