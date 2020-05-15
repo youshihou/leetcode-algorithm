cbad4561838d5165ece525136f046dc4d508583c
oneday <youshihou5@gmail.com>
2020-05-15 22:53:11 +0800

    T347-top-k-frequent-elements

---
+++ algorithm/algorithm/heap/T347-top-k-frequent-elements.c
@@ -0,0 +1,9 @@
+//
+//  T347-top-k-frequent-elements.c
+//  algorithm
+//
+//  Created by Ankui on 5/15/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T347-top-k-frequent-elements.h"
---
+++ algorithm/algorithm/heap/T347-top-k-frequent-elements.h
@@ -0,0 +1,14 @@
+//
+//  T347-top-k-frequent-elements.h
+//  algorithm
+//
+//  Created by Ankui on 5/15/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T347_top_k_frequent_elements_h
+#define T347_top_k_frequent_elements_h
+
+#include <stdio.h>
+
+#endif /* T347_top_k_frequent_elements_h */
