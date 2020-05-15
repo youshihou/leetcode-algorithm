f6555d00871dca3668d4b4de872f7bc0b7553600
oneday <youshihou5@gmail.com>
2020-05-15 22:53:18 +0800

    T347-top-k-frequent-elements

--- algorithm/algorithm/heap/T347-top-k-frequent-elements.c
+++ algorithm/algorithm/heap/T347-top-k-frequent-elements.c
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/top-k-frequent-elements/
+
 #include "T347-top-k-frequent-elements.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
+
+    return NULL;
+}
