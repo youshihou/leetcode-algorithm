b87c20fc7a549ba123e281683a66be31b7fd12ad
oneday <youshihou5@gmail.com>
2020-05-16 13:51:57 +0800

    add T36-valid-sudoku

--- algorithm/algorithm/weekly/T36-valid-sudoku.c
+++ algorithm/algorithm/weekly/T36-valid-sudoku.c
@@ -13,6 +13,27 @@
 
 
 bool isValidSudoku(char** board, int boardSize, int* boardColSize) {
+    if (board == NULL) { return false; }
+    
+    bool rows[9][9] = { false };
+    bool cols[9][9] = { false };
+    bool boxes[9][9] = { false };
+    
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            char num = board[row][col];
+            if (num == '.') { continue; }
+            num -= '1';
+            if (rows[row][num]) { return false; }
+            if (cols[col][num]) { return false; }
+            int idx = (row / 3) * 3 + col / 3;
+            if (boxes[idx][num]) { return false; }
+            
+            rows[row][num] = true;
+            cols[col][num] = true;
+            boxes[idx][num] = true;
+        }
+    }
 
-    return false;
+    return true;
 }
