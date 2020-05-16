999a3b04268899dd964297509f1dde8a948e6f61
oneday <youshihou5@gmail.com>
2020-05-16 12:50:01 +0800

    add T36-valid-sudoku

---
+++ algorithm/algorithm/weekly/T36-valid-sudoku.c
@@ -0,0 +1,18 @@
+//
+//  T36-valid-sudoku.c
+//  algorithm
+//
+//  Created by Ankui on 5/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+// https://leetcode-cn.com/problems/valid-sudoku/
+
+#include "T36-valid-sudoku.h"
+#include "algorithm-common.h"
+
+
+bool isValidSudoku(char** board, int boardSize, int* boardColSize) {
+
+    return false;
+}
---
+++ algorithm/algorithm/weekly/T36-valid-sudoku.h
@@ -0,0 +1,14 @@
+//
+//  T36-valid-sudoku.h
+//  algorithm
+//
+//  Created by Ankui on 5/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T36_valid_sudoku_h
+#define T36_valid_sudoku_h
+
+#include <stdio.h>
+
+#endif /* T36_valid_sudoku_h */
