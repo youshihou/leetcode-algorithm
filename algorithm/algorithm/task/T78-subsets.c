653d4748033497767392fcfae73db50b3edc301d
oneday <youshihou5@gmail.com>
2020-05-23 20:05:46 +0800

    add T78-subsets

--- algorithm/algorithm/task/T78-subsets.c
+++ algorithm/algorithm/task/T78-subsets.c
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/subsets/
+
 #include "T78-subsets.h"
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** subsets(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+
+    return NULL;
+}
--- algorithm/algorithm/task/T90-subsets-ii.c
+++ algorithm/algorithm/task/T90-subsets-ii.c
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/subsets-ii/
+
 #include "T90-subsets-ii.h"
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** subsetsWithDup(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+
+    return NULL;
+}
