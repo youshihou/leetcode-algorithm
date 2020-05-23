93994a728b6e743b0398ac76324fe245c0a5b8aa
oneday <youshihou5@gmail.com>
2020-05-23 17:19:35 +0800

    add T51-n-queens

--- algorithm/algorithm/task/T51-n-queens.c
+++ algorithm/algorithm/task/T51-n-queens.c
@@ -6,4 +6,58 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/n-queens/
+
 #include "T51-n-queens.h"
+#include "algorithm-common.h"
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+
+bool is_valid_(int row, int col, int* cols) {
+    for (int i = 0; i < row; i++) {
+        if (cols[i] == col) { return false; } // CARE!!!
+        if (row - i == abs(cols[i] - col)) { return false; } // CARE!!!
+    }
+    return true;
+}
+
+void place_(int n, int row, int* cols, char*** result, int* returnSize) {
+    if (row == n) {
+        result[*returnSize] = malloc(sizeof(char*) * n); // CARE!!!
+        for (int i = 0; i < n; i++) {
+            result[*returnSize][i] = calloc(n + 1, sizeof(char)); // CARE!!!
+            memset(result[*returnSize][i], '.', n);
+            result[*returnSize][i][cols[i]] = 'Q'; // CARE!!!
+        }
+        (*returnSize)++;
+        return;
+    }
+    
+    for (int col = 0; col < n; col++) {
+        if (is_valid_(row, col, cols)) {
+            cols[row] = col; // CARE!!!
+            place_(n, row + 1, cols, result, returnSize);
+        }
+    }
+}
+
+char *** solveNQueens(int n, int* returnSize, int** returnColumnSizes) {
+    *returnSize = 0;
+    if (n < 1) { return NULL; }
+    
+    int* cols = calloc(n, sizeof(int));
+    char*** result = malloc(sizeof(char**) * n * n * n);
+    place_(n, 0, cols, result, returnSize);
+    
+    *returnColumnSizes = malloc(sizeof(int) * *returnSize);
+    for (int i = 0; i < *returnSize; i++) {
+        (*returnColumnSizes)[i] = n;
+    }
+    
+    return result;
+}
--- algorithm/algorithm/task/T52-n-queens-ii.c
+++ algorithm/algorithm/task/T52-n-queens-ii.c
@@ -6,4 +6,43 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/n-queens-ii/
+
 #include "T52-n-queens-ii.h"
+#include "algorithm-common.h"
+
+bool is_valid(int row, int col, int* cols) {
+    for (int i = 0; i < row; i++) {
+        if (cols[i] == col) { return false; }
+        if (row - i == abs(col - cols[i])) { return false; }
+    }
+    
+    return true;
+}
+
+
+void place(int n, int row, int* cols, int* count) {
+    if (row == n) {
+        (*count)++;
+        return;
+    }
+    
+    for (int col = 0; col < n; col++) {
+        if (is_valid(row, col, cols)) {
+            cols[row] = col;
+            place(n, row + 1, cols, count);
+        }
+    }
+}
+
+int totalNQueens(int n) {
+    if (n < 1) { return 1; }
+    
+    int* cols = malloc(sizeof(int) * n);
+    memset(cols, 0, sizeof(int) * n);
+    int count = 0;
+    
+    place(n, 0, cols, &count);
+    
+    return count;
+}
