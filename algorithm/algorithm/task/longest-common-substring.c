30f2ee3fb9a0f00034e45e22c52b4994dcc27f50
oneday <youshihou5@gmail.com>
2020-05-24 22:04:06 +0800

    add longest-common-substring

---
+++ algorithm/algorithm/task/longest-common-substring.c
@@ -0,0 +1,9 @@
+//
+//  longest-common-substring.c
+//  algorithm
+//
+//  Created by Ankui on 5/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "longest-common-substring.h"
---
+++ algorithm/algorithm/task/longest-common-substring.h
@@ -0,0 +1,14 @@
+//
+//  longest-common-substring.h
+//  algorithm
+//
+//  Created by Ankui on 5/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef longest_common_substring_h
+#define longest_common_substring_h
+
+#include <stdio.h>
+
+#endif /* longest_common_substring_h */
