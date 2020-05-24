3208819585260803eee54e14dfe15698a2bca54b
oneday <youshihou5@gmail.com>
2020-05-24 19:59:28 +0800

    add T1143-longest-common-subsequence

---
+++ algorithm/algorithm/task/T1143-longest-common-subsequence.c
@@ -0,0 +1,9 @@
+//
+//  T1143-longest-common-subsequence.c
+//  algorithm
+//
+//  Created by Ankui on 5/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T1143-longest-common-subsequence.h"
---
+++ algorithm/algorithm/task/T1143-longest-common-subsequence.h
@@ -0,0 +1,14 @@
+//
+//  T1143-longest-common-subsequence.h
+//  algorithm
+//
+//  Created by Ankui on 5/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T1143_longest_common_subsequence_h
+#define T1143_longest_common_subsequence_h
+
+#include <stdio.h>
+
+#endif /* T1143_longest_common_subsequence_h */
