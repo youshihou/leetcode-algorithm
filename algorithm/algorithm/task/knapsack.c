a770bb92d2fa363685038bbbdd2345e47d70d57f
oneday <youshihou5@gmail.com>
2020-05-25 23:02:36 +0800

    add knapsack

---
+++ algorithm/algorithm/task/knapsack.c
@@ -0,0 +1,9 @@
+//
+//  knapsack.c
+//  algorithm
+//
+//  Created by Ankui on 5/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "knapsack.h"
---
+++ algorithm/algorithm/task/knapsack.h
@@ -0,0 +1,14 @@
+//
+//  knapsack.h
+//  algorithm
+//
+//  Created by Ankui on 5/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef knapsack_h
+#define knapsack_h
+
+#include <stdio.h>
+
+#endif /* knapsack_h */
