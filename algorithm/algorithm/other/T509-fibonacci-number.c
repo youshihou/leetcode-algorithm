f453e2c29964baf83722ced9ec87a974da2a6e9e
oneday <youshihou5@gmail.com>
2020-07-01 21:02:43 +0800

    add T509-fibonacci-number

--- algorithm/algorithm/other/T509-fibonacci-number.c
+++ algorithm/algorithm/other/T509-fibonacci-number.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/fibonacci-number/
+
 #include "T509-fibonacci-number.h"
+
+int fib(int N) {
+    if (N <= 1) { return N; }
+
+    int first = 0, second = 1;
+    for (int i = 0; i < N - 1; i++) {
+        second = first + second;
+        first = second - first;
+    }
+
+    return second;
+}
+
+int fib_(int N) {
+    if (N <= 1) { return N; }
+    
+    return fib(N - 1) + fib(N - 2);
+}
