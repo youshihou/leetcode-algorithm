176e6600b2b65965eab09c22c96336034dc9c54d
oneday <youshihou5@gmail.com>
2020-07-01 21:02:38 +0800

    add T509-fibonacci-number

---
+++ algorithm/algorithm/other/T509-fibonacci-number.c
@@ -0,0 +1,9 @@
+//
+//  T509-fibonacci-number.c
+//  algorithm
+//
+//  Created by Ankui on 7/1/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T509-fibonacci-number.h"
---
+++ algorithm/algorithm/other/T509-fibonacci-number.h
@@ -0,0 +1,14 @@
+//
+//  T509-fibonacci-number.h
+//  algorithm
+//
+//  Created by Ankui on 7/1/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T509_fibonacci_number_h
+#define T509_fibonacci_number_h
+
+#include <stdio.h>
+
+#endif /* T509_fibonacci_number_h */
