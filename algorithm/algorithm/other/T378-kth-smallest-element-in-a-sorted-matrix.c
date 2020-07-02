eb2da0cd2b765be59ed52058a75224296aa479ae
oneday <youshihou5@gmail.com>
2020-07-02 15:01:19 +0800

    add T378-kth-smallest-element-in-a-sorted-matrix

---
+++ algorithm/algorithm/other/T378-kth-smallest-element-in-a-sorted-matrix.c
@@ -0,0 +1,9 @@
+//
+//  T378-kth-smallest-element-in-a-sorted-matrix.c
+//  algorithm
+//
+//  Created by Ankui on 7/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T378-kth-smallest-element-in-a-sorted-matrix.h"
---
+++ algorithm/algorithm/other/T378-kth-smallest-element-in-a-sorted-matrix.h
@@ -0,0 +1,14 @@
+//
+//  T378-kth-smallest-element-in-a-sorted-matrix.h
+//  algorithm
+//
+//  Created by Ankui on 7/2/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T378_kth_smallest_element_in_a_sorted_matrix_h
+#define T378_kth_smallest_element_in_a_sorted_matrix_h
+
+#include <stdio.h>
+
+#endif /* T378_kth_smallest_element_in_a_sorted_matrix_h */
