42bcc517f1b556c427754b837de7379bbdf16c10
oneday <youshihou5@gmail.com>
2020-07-04 22:15:59 +0800

    add T32-longest-valid-parentheses

--- algorithm/algorithm/other/T32-longest-valid-parentheses.c
+++ algorithm/algorithm/other/T32-longest-valid-parentheses.c
@@ -16,11 +16,38 @@ int longestValidParentheses(char * s) {
     if (s == NULL) { return 0; }
     size_t len = strlen(s);
     if (len == 0) { return 0; }
+    int maxlen = 0;
+    int stack[len + 1];
+    int top = -1;
+    stack[++top] = -1;
+    
+    for (int i = 0; i < len; i++) {
+        if (s[i] == '(') {
+            stack[++top] = i;
+        } else {
+            --top;
+            if (top == -1) {
+                stack[++top] = i;
+            } else {
+                maxlen = fmax(maxlen, i - stack[top]);
+            }
+        }
+    }
+    
+    return maxlen;
+}
+
+
+
+int longestValidParentheses1(char * s) {
+    if (s == NULL) { return 0; }
+    size_t len = strlen(s);
+    if (len == 0) { return 0; }
     int dp[len];
     memset(dp, 0, sizeof(dp));
     int maxlen = 0;
     
-    for (int i = 1; i < len; i++) {
+    for (int i = 1; i < len; i++) { // CARE!!! i = 1 not i = 0
         if (s[i] == ')') {
             if (s[i - 1] == '(') {
                 dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
