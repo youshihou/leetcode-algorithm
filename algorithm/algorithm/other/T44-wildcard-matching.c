0f4b026696d6197fd1df1673da6e3f01e07774ba
oneday <youshihou5@gmail.com>
2020-07-05 18:18:35 +0800

    add T44-wildcard-matching

---
+++ algorithm/algorithm/other/T44-wildcard-matching.c
@@ -0,0 +1,9 @@
+//
+//  T44-wildcard-matching.c
+//  algorithm
+//
+//  Created by Ankui on 7/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T44-wildcard-matching.h"
---
+++ algorithm/algorithm/other/T44-wildcard-matching.h
@@ -0,0 +1,14 @@
+//
+//  T44-wildcard-matching.h
+//  algorithm
+//
+//  Created by Ankui on 7/5/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T44_wildcard_matching_h
+#define T44_wildcard_matching_h
+
+#include <stdio.h>
+
+#endif /* T44_wildcard_matching_h */
