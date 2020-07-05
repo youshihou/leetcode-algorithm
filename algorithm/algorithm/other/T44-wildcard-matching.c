14480baf456ca0802670863e07a7cc8dc9d96e78
oneday <youshihou5@gmail.com>
2020-07-05 18:18:39 +0800

    add T44-wildcard-matching

--- algorithm/algorithm/other/T44-wildcard-matching.c
+++ algorithm/algorithm/other/T44-wildcard-matching.c
@@ -6,4 +6,37 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/wildcard-matching/
+
 #include "T44-wildcard-matching.h"
+#include "algorithm-common.h"
+
+
+bool isMatch(char * s, char * p) {
+    if (s == NULL || p == NULL) { return false; }
+    size_t slen = strlen(s);
+    size_t plen = strlen(p);
+    int dp[slen + 1][plen + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[0][0] = true;
+    
+    for (int i = 1; i <= plen; i++) {
+        if (p[i - 1] == '*') {
+            dp[0][i] = true;
+        } else {
+            break;
+        }
+    }
+    
+    for (int i = 1; i <= slen; i++) {
+        for (int j = 1; j <= plen; j++) {
+            if (p[j - 1] == '*') {
+                dp[i][j] = dp[i][j - 1] | dp[i - 1][j];
+            } else if (p[j - 1] == '?' || s[i - 1] == p[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+        }
+    }
+
+    return dp[slen][plen];
+}
