2d19c795d0f2a1665f210145f689c397d3fce3ac
oneday <youshihou5@gmail.com>
2020-07-08 09:26:41 +0800

    add T16-11-diving-board-lcci

---
+++ algorithm/algorithm/other/T16-11-diving-board-lcci.c
@@ -0,0 +1,9 @@
+//
+//  T16-11-diving-board-lcci.c
+//  algorithm
+//
+//  Created by Ankui on 7/8/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T16-11-diving-board-lcci.h"
---
+++ algorithm/algorithm/other/T16-11-diving-board-lcci.h
@@ -0,0 +1,14 @@
+//
+//  T16-11-diving-board-lcci.h
+//  algorithm
+//
+//  Created by Ankui on 7/8/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T16_11_diving_board_lcci_h
+#define T16_11_diving_board_lcci_h
+
+#include <stdio.h>
+
+#endif /* T16_11_diving_board_lcci_h */
