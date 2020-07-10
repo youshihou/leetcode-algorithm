96077a2e3525e4910f5a2ff67f20ab167e62e997
oneday <youshihou5@gmail.com>
2020-07-10 09:39:30 +0800

    T309-best-time-to-buy-and-sell-stock-with-cooldown

--- algorithm/algorithm/other/T309-best-time-to-buy-and-sell-stock-with-cooldown.c
+++ algorithm/algorithm/other/T309-best-time-to-buy-and-sell-stock-with-cooldown.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+
+
 #include "T309-best-time-to-buy-and-sell-stock-with-cooldown.h"
+#include "algorithm-common.h"
+
+int maxProfit(int* prices, int pricesSize) {
+    int dp_0 = 0;
+    int dp_1 = INT_MIN;
+    int dp_pre = 0;
+    int tmp = 0;
+    
+    for (int i = 0; i < pricesSize; i++) {
+        tmp = dp_0;
+        dp_0 = fmax(dp_0, dp_1 + prices[i]);
+        dp_1 = fmax(dp_1, dp_pre - prices[i]);
+        dp_pre = tmp;
+    }
+
+    return dp_0;
+}
