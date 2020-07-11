2a98810bfcadfa7c42604ffe3437a50e057d7e28
oneday <youshihou5@gmail.com>
2020-07-11 18:57:14 +0800

    T315-count-of-smaller-numbers-after-self

---
+++ algorithm/algorithm/other/T315-count-of-smaller-numbers-after-self.c
@@ -0,0 +1,9 @@
+//
+//  T315-count-of-smaller-numbers-after-self.c
+//  algorithm
+//
+//  Created by Ankui on 7/11/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T315-count-of-smaller-numbers-after-self.h"
---
+++ algorithm/algorithm/other/T315-count-of-smaller-numbers-after-self.h
@@ -0,0 +1,14 @@
+//
+//  T315-count-of-smaller-numbers-after-self.h
+//  algorithm
+//
+//  Created by Ankui on 7/11/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T315_count_of_smaller_numbers_after_self_h
+#define T315_count_of_smaller_numbers_after_self_h
+
+#include <stdio.h>
+
+#endif /* T315_count_of_smaller_numbers_after_self_h */
