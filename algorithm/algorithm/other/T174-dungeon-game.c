3661cf9e70c3dd4ccfde4e95faaa4a24d802f692
oneday <youshihou5@gmail.com>
2020-07-12 13:15:07 +0800

    T174-dungeon-game

---
+++ algorithm/algorithm/other/T174-dungeon-game.c
@@ -0,0 +1,9 @@
+//
+//  T174-dungeon-game.c
+//  algorithm
+//
+//  Created by Ankui on 7/12/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T174-dungeon-game.h"
---
+++ algorithm/algorithm/other/T174-dungeon-game.h
@@ -0,0 +1,14 @@
+//
+//  T174-dungeon-game.h
+//  algorithm
+//
+//  Created by Ankui on 7/12/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T174_dungeon_game_h
+#define T174_dungeon_game_h
+
+#include <stdio.h>
+
+#endif /* T174_dungeon_game_h */
