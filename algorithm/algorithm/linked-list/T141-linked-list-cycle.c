5af5c9d48cb7b15464334fffe5d17fd38f000105
oneday <youshihou5@gmail.com>
2020-07-13 23:21:36 +0800

    add T141-linked-list-cycle

--- algorithm/algorithm/linked-list/T141-linked-list-cycle.c
+++ algorithm/algorithm/linked-list/T141-linked-list-cycle.c
@@ -6,4 +6,29 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/linked-list-cycle/
+
 #include "T141-linked-list-cycle.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    if (head == NULL || head->next == NULL) { return false; }
+    
+    struct ListNode* slow = head;
+    struct ListNode* fast = head->next;
+    
+    while (fast != NULL && fast->next != NULL) { // CARE!!! 
+        if (slow == fast) { return true; }
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return false;
+}
