4a9d79bf9dfed34a8a24bc6994d2663f950bad51
oneday <youshihou5@gmail.com>
2020-07-13 09:36:33 +0800

    T350-intersection-of-two-arrays-ii

---
+++ algorithm/algorithm/other/T350-intersection-of-two-arrays-ii.c
@@ -0,0 +1,9 @@
+//
+//  T350-intersection-of-two-arrays-ii.c
+//  algorithm
+//
+//  Created by Ankui on 7/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T350-intersection-of-two-arrays-ii.h"
---
+++ algorithm/algorithm/other/T350-intersection-of-two-arrays-ii.h
@@ -0,0 +1,14 @@
+//
+//  T350-intersection-of-two-arrays-ii.h
+//  algorithm
+//
+//  Created by Ankui on 7/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T350_intersection_of_two_arrays_ii_h
+#define T350_intersection_of_two_arrays_ii_h
+
+#include <stdio.h>
+
+#endif /* T350_intersection_of_two_arrays_ii_h */
