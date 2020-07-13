0326f326ce713caf16072ab6e02fe01cd1fc1917
oneday <youshihou5@gmail.com>
2020-07-13 23:25:28 +0800

    add T141-linked-list-cycle

--- algorithm/algorithm/linked-list/T141-linked-list-cycle.c
+++ algorithm/algorithm/linked-list/T141-linked-list-cycle.c
@@ -25,9 +25,9 @@ bool hasCycle(struct ListNode *head) {
     struct ListNode* fast = head->next;
     
     while (fast != NULL && fast->next != NULL) { // CARE!!! 
-        if (slow == fast) { return true; }
         slow = slow->next;
         fast = fast->next->next;
+        if (slow == fast) { return true; }
     }
     
     return false;
