3b8c1fc431a3df6313956d64754b15b02c760e26
oneday <youshihou5@gmail.com>
2020-07-13 09:36:38 +0800

    T350-intersection-of-two-arrays-ii

--- algorithm/algorithm/other/T350-intersection-of-two-arrays-ii.c
+++ algorithm/algorithm/other/T350-intersection-of-two-arrays-ii.c
@@ -6,4 +6,49 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
+
+
 #include "T350-intersection-of-two-arrays-ii.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int cmp_t350(const void* a, const void* b) {
+    int *_a = (int*)a;
+    int *_b = (int*)b;
+    if (*_a == *_b) {
+        return 0;
+    } else if (*_a > *_b) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    *returnSize = 0;
+    if (nums1 == NULL || nums1Size <= 0 || nums2 == NULL || nums2Size <= 0) { return NULL; }
+    
+    qsort(nums1, nums1Size, sizeof(int), cmp_t350);
+    qsort(nums2, nums2Size, sizeof(int), cmp_t350);
+    int *result = malloc(sizeof(int) * fmin(nums1Size, nums2Size));
+    
+    int idx1 = 0, idx2 = 0;
+    while (idx1 < nums1Size && idx2 < nums2Size) {
+        if (nums1[idx1] < nums2[idx2]) {
+            idx1++;
+        } else if (nums1[idx1] > nums2[idx2]) {
+            idx2++;
+        } else {
+            result[(*returnSize)++] = nums1[idx1];
+            idx1++;
+            idx2++;
+        }
+    }
+    
+    return result;
+}
