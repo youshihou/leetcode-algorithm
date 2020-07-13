46a86fa4ddffc3027e32f5774388d64d41406230
oneday <youshihou5@gmail.com>
2020-07-13 22:57:15 +0800

    add T206-reverse-linked-list

---
+++ algorithm/algorithm/linked-list/T206-reverse-linked-list.c
@@ -0,0 +1,9 @@
+//
+//  T206-reverse-linked-list.c
+//  algorithm
+//
+//  Created by Ankui on 7/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T206-reverse-linked-list.h"
---
+++ algorithm/algorithm/linked-list/T206-reverse-linked-list.h
@@ -0,0 +1,14 @@
+//
+//  T206-reverse-linked-list.h
+//  algorithm
+//
+//  Created by Ankui on 7/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T206_reverse_linked_list_h
+#define T206_reverse_linked_list_h
+
+#include <stdio.h>
+
+#endif /* T206_reverse_linked_list_h */
