9b5817e8d6d4fb6ca1daead5cd72ac2c4538e241
oneday <youshihou5@gmail.com>
2020-07-13 22:19:45 +0800

    add T237-delete-node-in-a-linked-list

---
+++ algorithm/algorithm/linked-list/T237-delete-node-in-a-linked-list.c
@@ -0,0 +1,9 @@
+//
+//  T237-delete-node-in-a-linked-list.c
+//  algorithm
+//
+//  Created by Ankui on 7/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T237-delete-node-in-a-linked-list.h"
---
+++ algorithm/algorithm/linked-list/T237-delete-node-in-a-linked-list.h
@@ -0,0 +1,14 @@
+//
+//  T237-delete-node-in-a-linked-list.h
+//  algorithm
+//
+//  Created by Ankui on 7/13/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T237_delete_node_in_a_linked_list_h
+#define T237_delete_node_in_a_linked_list_h
+
+#include <stdio.h>
+
+#endif /* T237_delete_node_in_a_linked_list_h */
