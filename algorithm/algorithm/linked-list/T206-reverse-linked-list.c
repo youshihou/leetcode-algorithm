1caf90f75b3880222ba101d7f84cbeba82a5fa24
oneday <youshihou5@gmail.com>
2020-07-13 22:57:19 +0800

    add T206-reverse-linked-list

--- algorithm/algorithm/linked-list/T206-reverse-linked-list.c
+++ algorithm/algorithm/linked-list/T206-reverse-linked-list.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/reverse-linked-list/
+
 #include "T206-reverse-linked-list.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) { return head; }
+    
+    struct ListNode* newHead = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    
+    return newHead;
+}
