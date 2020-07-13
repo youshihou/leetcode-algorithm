31dc34164b6d09570208d3ceb46c3959f154654f
oneday <youshihou5@gmail.com>
2020-07-13 22:19:51 +0800

    add T237-delete-node-in-a-linked-list

--- algorithm/algorithm/linked-list/T237-delete-node-in-a-linked-list.c
+++ algorithm/algorithm/linked-list/T237-delete-node-in-a-linked-list.c
@@ -6,4 +6,20 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+
+
 #include "T237-delete-node-in-a-linked-list.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
