e549b80c400514e0af5dffb3606dbb6016905b48
oneday <youshihou5@gmail.com>
2020-07-14 21:24:20 +0800

    add T876-middle-of-the-linked-list

--- algorithm/algorithm/linked-list/T876-middle-of-the-linked-list.c
+++ algorithm/algorithm/linked-list/T876-middle-of-the-linked-list.c
@@ -6,4 +6,29 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/middle-of-the-linked-list/
+
+
 #include "T876-middle-of-the-linked-list.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* middleNode(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) { return head; }
+    
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
