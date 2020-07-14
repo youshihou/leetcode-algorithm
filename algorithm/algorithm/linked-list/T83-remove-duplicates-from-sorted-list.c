1a1229b0482580c0d1f4f017988f6a4fbf3e44f5
oneday <youshihou5@gmail.com>
2020-07-14 20:51:22 +0800

    add T83-remove-duplicates-from-sorted-list

---
+++ algorithm/algorithm/linked-list/T83-remove-duplicates-from-sorted-list.c
@@ -0,0 +1,9 @@
+//
+//  T83-remove-duplicates-from-sorted-list.c
+//  algorithm
+//
+//  Created by Ankui on 7/14/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T83-remove-duplicates-from-sorted-list.h"
---
+++ algorithm/algorithm/linked-list/T83-remove-duplicates-from-sorted-list.h
@@ -0,0 +1,14 @@
+//
+//  T83-remove-duplicates-from-sorted-list.h
+//  algorithm
+//
+//  Created by Ankui on 7/14/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T83_remove_duplicates_from_sorted_list_h
+#define T83_remove_duplicates_from_sorted_list_h
+
+#include <stdio.h>
+
+#endif /* T83_remove_duplicates_from_sorted_list_h */
