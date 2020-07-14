97b085e14b2114c1b670e9fbd15282977bce7eaf
oneday <youshihou5@gmail.com>
2020-07-14 21:24:16 +0800

    add T876-middle-of-the-linked-list

---
+++ algorithm/algorithm/linked-list/T876-middle-of-the-linked-list.c
@@ -0,0 +1,9 @@
+//
+//  T876-middle-of-the-linked-list.c
+//  algorithm
+//
+//  Created by Ankui on 7/14/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T876-middle-of-the-linked-list.h"
---
+++ algorithm/algorithm/linked-list/T876-middle-of-the-linked-list.h
@@ -0,0 +1,14 @@
+//
+//  T876-middle-of-the-linked-list.h
+//  algorithm
+//
+//  Created by Ankui on 7/14/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T876_middle_of_the_linked_list_h
+#define T876_middle_of_the_linked_list_h
+
+#include <stdio.h>
+
+#endif /* T876_middle_of_the_linked_list_h */
