4f549fab4ef5fdb6ef7c02038ed8f3533e217bac
oneday <youshihou5@gmail.com>
2020-07-14 20:51:26 +0800

    add T83-remove-duplicates-from-sorted-list

--- algorithm/algorithm/linked-list/T83-remove-duplicates-from-sorted-list.c
+++ algorithm/algorithm/linked-list/T83-remove-duplicates-from-sorted-list.c
@@ -6,4 +6,34 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+
+
 #include "T83-remove-duplicates-from-sorted-list.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) { return head; }
+    
+    struct ListNode* curr = head;
+    while (curr != NULL && curr->next != NULL) {
+        if (curr->next->val == curr->val) {
+            struct ListNode *tmp = curr->next;
+            curr->next = curr->next->next;
+            free(tmp);
+        } else {
+            curr = curr->next;
+        }
+    }
+    
+    return head;
+}
