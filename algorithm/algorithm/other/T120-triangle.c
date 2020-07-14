cfccc007b6756d47dbbce336e4e57e2ef8726cdc
oneday <youshihou5@gmail.com>
2020-07-14 15:52:36 +0800

    add T120-triangle

---
+++ algorithm/algorithm/other/T120-triangle.c
@@ -0,0 +1,9 @@
+//
+//  T120-triangle.c
+//  algorithm
+//
+//  Created by Ankui on 7/14/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T120-triangle.h"
---
+++ algorithm/algorithm/other/T120-triangle.h
@@ -0,0 +1,14 @@
+//
+//  T120-triangle.h
+//  algorithm
+//
+//  Created by Ankui on 7/14/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T120_triangle_h
+#define T120_triangle_h
+
+#include <stdio.h>
+
+#endif /* T120_triangle_h */
