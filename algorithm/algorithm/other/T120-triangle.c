af5420b7b6f322128e8007845426176245a41401
oneday <youshihou5@gmail.com>
2020-07-14 17:42:53 +0800

    add T120-triangle

--- algorithm/algorithm/other/T120-triangle.c
+++ algorithm/algorithm/other/T120-triangle.c
@@ -15,6 +15,31 @@
 int minimumTotal(int** triangle, int triangleSize, int* triangleColSize) {
     if (triangle == NULL || triangleSize <= 0) { return 0; }
     
+    int dp[2][triangleSize];
+    memset(dp, 0, sizeof(dp));
+    dp[0][0] = triangle[0][0];
+    for (int i = 1; i < triangleSize; i++) {
+        int curr = i % 2;
+        int prev = 1 - curr;
+        dp[curr][0] = dp[prev][0] + triangle[i][0];
+        for (int j = 1; j < i; j++) {
+            dp[curr][j] = fmin(dp[prev][j - 1], dp[prev][j]) + triangle[i][j];
+        }
+        dp[curr][i] = dp[prev][i - 1] + triangle[i][i];
+    }
+    int ret = dp[(triangleSize - 1) % 2][0];
+    for (int i = 1; i < triangleSize; i++) {
+        ret = fmin(ret, dp[(triangleSize - 1) % 2][i]);
+    }
+    return ret;
+}
+
+
+
+
+int minimumTotal1(int** triangle, int triangleSize, int* triangleColSize) {
+    if (triangle == NULL || triangleSize <= 0) { return 0; }
+    
     int dp[triangleSize][triangleSize];
     memset(dp, 0, sizeof(dp));
     dp[0][0] = triangle[0][0];
