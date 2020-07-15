9ecbbeaeadedcfcf997096491dfcc7fa34bae85f
oneday <youshihou5@gmail.com>
2020-07-15 09:02:39 +0800

    add T96-unique-binary-search-trees

--- algorithm/algorithm/other/T96-unique-binary-search-trees.c
+++ algorithm/algorithm/other/T96-unique-binary-search-trees.c
@@ -6,4 +6,21 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/unique-binary-search-trees/
+
+
 #include "T96-unique-binary-search-trees.h"
+#include "algorithm-common.h"
+
+int numTrees(int n) {
+    int dp[n + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[0] = dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i] += dp[j - 1] * dp[i - j];
+        }
+    }
+    
+    return dp[n];
+}
