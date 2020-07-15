9282b920651ff789a18569f84aa18ea097890c7e
oneday <youshihou5@gmail.com>
2020-07-15 09:02:34 +0800

    add T96-unique-binary-search-trees

---
+++ algorithm/algorithm/other/T96-unique-binary-search-trees.c
@@ -0,0 +1,9 @@
+//
+//  T96-unique-binary-search-trees.c
+//  algorithm
+//
+//  Created by Ankui on 7/15/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T96-unique-binary-search-trees.h"
---
+++ algorithm/algorithm/other/T96-unique-binary-search-trees.h
@@ -0,0 +1,14 @@
+//
+//  T96-unique-binary-search-trees.h
+//  algorithm
+//
+//  Created by Ankui on 7/15/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T96_unique_binary_search_trees_h
+#define T96_unique_binary_search_trees_h
+
+#include <stdio.h>
+
+#endif /* T96_unique_binary_search_trees_h */
