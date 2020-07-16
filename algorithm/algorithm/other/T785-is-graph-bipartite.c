2efb283e56ee40d4b38b9ebb89136c8647480982
oneday <youshihou5@gmail.com>
2020-07-16 09:24:06 +0800

    add T785-is-graph-bipartite

--- algorithm/algorithm/other/T785-is-graph-bipartite.c
+++ algorithm/algorithm/other/T785-is-graph-bipartite.c
@@ -6,4 +6,39 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/is-graph-bipartite/
+
+
 #include "T785-is-graph-bipartite.h"
+#include "algorithm-common.h"
+
+bool dfs(int** graph, int* graphColSize, int node, int c, int* color) {
+    color[node] = c;
+    int cNei = (c == 1 ? 2 : 1);
+    for (int i = 0; i < graphColSize[node]; i++) {
+        int neighbor = graph[node][i];
+        if (color[neighbor] == 0) {
+            if (!dfs(graph, graphColSize, neighbor, cNei, color)) {
+                return false;
+            }
+        } else if (color[neighbor] != cNei) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isBipartite(int** graph, int graphSize, int* graphColSize) {
+    int* color = malloc(sizeof(int) * graphSize);
+    memset(color, 0, sizeof(int) * graphSize);
+    for (int i = 0; i < graphSize; i++) {
+        if (color[i] == 0) {
+            if (!dfs(graph, graphColSize, i, 1, color)) {
+                free(color);
+                return false;
+            }
+        }
+    }
+    free(color);
+    return true;
+}
