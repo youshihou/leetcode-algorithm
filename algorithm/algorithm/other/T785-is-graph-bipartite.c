44fffdf23b7d81e2a14708851fd9b7f47e6b2569
oneday <youshihou5@gmail.com>
2020-07-16 09:24:01 +0800

    add T785-is-graph-bipartite

---
+++ algorithm/algorithm/other/T785-is-graph-bipartite.c
@@ -0,0 +1,9 @@
+//
+//  T785-is-graph-bipartite.c
+//  algorithm
+//
+//  Created by Ankui on 7/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T785-is-graph-bipartite.h"
---
+++ algorithm/algorithm/other/T785-is-graph-bipartite.h
@@ -0,0 +1,14 @@
+//
+//  T785-is-graph-bipartite.h
+//  algorithm
+//
+//  Created by Ankui on 7/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T785_is_graph_bipartite_h
+#define T785_is_graph_bipartite_h
+
+#include <stdio.h>
+
+#endif /* T785_is_graph_bipartite_h */
