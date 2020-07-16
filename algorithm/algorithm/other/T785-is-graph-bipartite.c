80fdc98f079c038bc9ab901a0abd95bc420cb954
oneday <youshihou5@gmail.com>
2020-07-16 09:30:33 +0800

    add T785-is-graph-bipartite

--- algorithm/algorithm/other/T785-is-graph-bipartite.c
+++ algorithm/algorithm/other/T785-is-graph-bipartite.c
@@ -12,13 +12,47 @@
 #include "T785-is-graph-bipartite.h"
 #include "algorithm-common.h"
 
-bool dfs(int** graph, int* graphColSize, int node, int c, int* color) {
+bool isBipartite(int** graph, int graphSize, int* graphColSize) {
+    int* color = malloc(sizeof(int) * graphSize);
+    memset(color, 0, sizeof(int) * graphSize);
+    
+    int* q = malloc(sizeof(int) * graphSize);
+    for (int i = 0; i < graphSize; i++) {
+        if (color[i] == 0) {
+            int l = 0;
+            int r = 0;
+            q[0] = i;
+            color[i] = 1;
+            while (l <= r) {
+                int node = q[l++];
+                int cNei = (color[node] == 1 ? 2 : 1);
+                for (int j = 0; j < graphColSize[node]; j++) {
+                    int neighbor = graph[node][j];
+                    if (color[neighbor] == 0) {
+                        q[++r] = neighbor;
+                        color[neighbor] = cNei;
+                    } else if (color[neighbor] != cNei) {
+                        free(color);
+                        free(q);
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    free(color);
+    free(q);
+    return true;
+}
+
+
+bool dfs_t785(int** graph, int* graphColSize, int node, int c, int* color) {
     color[node] = c;
     int cNei = (c == 1 ? 2 : 1);
     for (int i = 0; i < graphColSize[node]; i++) {
         int neighbor = graph[node][i];
         if (color[neighbor] == 0) {
-            if (!dfs(graph, graphColSize, neighbor, cNei, color)) {
+            if (!dfs_t785(graph, graphColSize, neighbor, cNei, color)) {
                 return false;
             }
         } else if (color[neighbor] != cNei) {
@@ -28,12 +62,12 @@ bool dfs(int** graph, int* graphColSize, int node, int c, int* color) {
     return true;
 }
 
-bool isBipartite(int** graph, int graphSize, int* graphColSize) {
+bool isBipartite1(int** graph, int graphSize, int* graphColSize) {
     int* color = malloc(sizeof(int) * graphSize);
     memset(color, 0, sizeof(int) * graphSize);
     for (int i = 0; i < graphSize; i++) {
         if (color[i] == 0) {
-            if (!dfs(graph, graphColSize, i, 1, color)) {
+            if (!dfs_t785(graph, graphColSize, i, 1, color)) {
                 free(color);
                 return false;
             }
