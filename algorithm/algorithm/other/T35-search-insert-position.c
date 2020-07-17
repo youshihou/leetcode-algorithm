aa46f6e6ecea21340caf0bcab9360e4e51dc64c5
oneday <youshihou5@gmail.com>
2020-07-17 09:18:50 +0800

    add T35-search-insert-position

--- algorithm/algorithm/other/T35-search-insert-position.c
+++ algorithm/algorithm/other/T35-search-insert-position.c
@@ -6,4 +6,31 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/search-insert-position/
+
+
 #include "T35-search-insert-position.h"
+#include "algorithm-common.h"
+
+int searchInsert(int* nums, int numsSize, int target) {
+    if (nums == NULL || numsSize <= 0) { return 0; }
+    
+    int begin = 0;
+    int end = numsSize;
+    while (begin < end) {
+        int mid = (begin + end) >> 1;
+        int num = nums[mid];
+        if (num == target) {
+            return mid;
+        } else if (num > target) {
+            end = mid;
+        } else {
+            begin = mid + 1;
+        }
+    }
+    
+    return end;
+}
+
+
+
