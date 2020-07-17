757b8be1349220a4aaf4f06e6014358565ec6e43
oneday <youshihou5@gmail.com>
2020-07-17 09:18:46 +0800

    add T35-search-insert-position

---
+++ algorithm/algorithm/other/T35-search-insert-position.c
@@ -0,0 +1,9 @@
+//
+//  T35-search-insert-position.c
+//  algorithm
+//
+//  Created by Ankui on 7/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T35-search-insert-position.h"
---
+++ algorithm/algorithm/other/T35-search-insert-position.h
@@ -0,0 +1,14 @@
+//
+//  T35-search-insert-position.h
+//  algorithm
+//
+//  Created by Ankui on 7/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T35_search_insert_position_h
+#define T35_search_insert_position_h
+
+#include <stdio.h>
+
+#endif /* T35_search_insert_position_h */
