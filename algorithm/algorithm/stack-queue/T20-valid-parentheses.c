a0e38a741c5fc3ef300088b80d18a1a5fbd991f7
oneday <youshihou5@gmail.com>
2020-07-18 18:01:33 +0800

    add T20-valid-parentheses

--- algorithm/algorithm/stack-queue/T20-valid-parentheses.c
+++ algorithm/algorithm/stack-queue/T20-valid-parentheses.c
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/valid-parentheses/
+
 #include "T20-valid-parentheses.h"
+#include "algorithm-common.h"
+
+bool isValid(char * s) {
+    if (s == NULL) { return false; }
+    size_t len = strlen(s);
+    
+    Stack* stack = create();
+    
+    for (int i = 0; i < len; i++) {
+        char c = s[i];
+        if (c == '{' || c == '[' || c == '(') {
+            push(stack, c);
+        } else {
+            if (isEmpty(stack)) { return false; }
+            
+            char left = pop(stack);
+            if (left == '{' && c != '}') { return false; }
+            if (left == '[' && c != ']') { return false; }
+            if (left == '(' && c != ')') { return false; }
+        }
+    }
+    
+    return isEmpty(stack);
+}
