3e368d68709f2b1c64a5cd52573e2606cadf72b9
oneday <youshihou5@gmail.com>
2020-07-18 19:02:03 +0800

    add T97-interleaving-string

--- algorithm/algorithm/other/T97-interleaving-string.c
+++ algorithm/algorithm/other/T97-interleaving-string.c
@@ -19,6 +19,32 @@ bool isInterleave(char * s1, char * s2, char * s3) {
     size_t t = strlen(s3);
     if (n + m != t) { return false; }
     
+    int dp[m + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[0] = true;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            int p = i + j - 1;
+            if (i > 0) {
+                dp[j] &= (s1[i - 1] == s3[p]);
+            }
+            if (j > 0) {
+                dp[j] |= (dp[j - 1] && s2[j - 1] == s3[p]);
+            }
+        }
+    }
+    
+    return dp[m];
+}
+
+
+
+bool isInterleave1(char * s1, char * s2, char * s3) {
+    size_t n = strlen(s1);
+    size_t m = strlen(s2);
+    size_t t = strlen(s3);
+    if (n + m != t) { return false; }
+    
     int dp[n + 1][m + 1];
     memset(dp, 0, sizeof(dp));
     dp[0][0] = true;
