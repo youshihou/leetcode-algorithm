511c61bce01741d4b5e2991411fa02dd216d9b52
oneday <youshihou5@gmail.com>
2020-07-19 00:12:04 +0800

    add T232-implement-queue-using-stacks

---
+++ algorithm/algorithm/stack-queue/T232-implement-queue-using-stacks.c
@@ -0,0 +1,9 @@
+//
+//  T232-implement-queue-using-stacks.c
+//  algorithm
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T232-implement-queue-using-stacks.h"
---
+++ algorithm/algorithm/stack-queue/T232-implement-queue-using-stacks.h
@@ -0,0 +1,14 @@
+//
+//  T232-implement-queue-using-stacks.h
+//  algorithm
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T232_implement_queue_using_stacks_h
+#define T232_implement_queue_using_stacks_h
+
+#include <stdio.h>
+
+#endif /* T232_implement_queue_using_stacks_h */
