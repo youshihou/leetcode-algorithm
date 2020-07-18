026701e18ddc49a9c9a77dab93c67dc2c085d6d0
oneday <youshihou5@gmail.com>
2020-07-18 18:13:14 +0800

    add T856-score-of-parentheses

---
+++ algorithm/algorithm/stack-queue/T856-score-of-parentheses.c
@@ -0,0 +1,9 @@
+//
+//  T856-score-of-parentheses.c
+//  algorithm
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T856-score-of-parentheses.h"
---
+++ algorithm/algorithm/stack-queue/T856-score-of-parentheses.h
@@ -0,0 +1,14 @@
+//
+//  T856-score-of-parentheses.h
+//  algorithm
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T856_score_of_parentheses_h
+#define T856_score_of_parentheses_h
+
+#include <stdio.h>
+
+#endif /* T856_score_of_parentheses_h */
