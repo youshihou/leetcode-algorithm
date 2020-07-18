c9eb416b1ba367405c0cf37aca7e9a4df16c7668
oneday <youshihou5@gmail.com>
2020-07-18 18:14:41 +0800

    add T224-basic-calculator

---
+++ algorithm/algorithm/stack-queue/T224-basic-calculator.c
@@ -0,0 +1,9 @@
+//
+//  T224-basic-calculator.c
+//  algorithm
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T224-basic-calculator.h"
---
+++ algorithm/algorithm/stack-queue/T224-basic-calculator.h
@@ -0,0 +1,14 @@
+//
+//  T224-basic-calculator.h
+//  algorithm
+//
+//  Created by Ankui on 7/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T224_basic_calculator_h
+#define T224_basic_calculator_h
+
+#include <stdio.h>
+
+#endif /* T224_basic_calculator_h */
