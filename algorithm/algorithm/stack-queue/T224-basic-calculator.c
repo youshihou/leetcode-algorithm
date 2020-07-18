c8f88c5b5ae0188c1ce7910805dcb0bc24c8f85a
oneday <youshihou5@gmail.com>
2020-07-18 18:14:46 +0800

    add T224-basic-calculator

--- algorithm/algorithm/stack-queue/T224-basic-calculator.c
+++ algorithm/algorithm/stack-queue/T224-basic-calculator.c
@@ -6,4 +6,14 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/basic-calculator/
+
+
 #include "T224-basic-calculator.h"
+#include "algorithm-common.h"
+
+
+int calculate(char * s) {
+
+    return 0;
+}
