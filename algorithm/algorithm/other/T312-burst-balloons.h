4c28a4c90cc84a5bffb087b1be5be06291aa8ebd
oneday <youshihou5@gmail.com>
2020-07-19 20:29:14 +0800

    add T312-burst-balloons

---
+++ algorithm/algorithm/other/T312-burst-balloons.h
@@ -0,0 +1,14 @@
+//
+//  T312-burst-balloons.h
+//  algorithm
+//
+//  Created by Ankui on 7/19/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T312_burst_balloons_h
+#define T312_burst_balloons_h
+
+#include <stdio.h>
+
+#endif /* T312_burst_balloons_h */
