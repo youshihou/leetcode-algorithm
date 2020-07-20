ed70357180ab399457e4c1c88e0b8cea0dc16ca4
oneday <youshihou5@gmail.com>
2020-07-20 09:45:59 +0800

    add T167-two-sum-ii-input-array-is-sorted

--- algorithm/algorithm/other/T167-two-sum-ii-input-array-is-sorted.c
+++ algorithm/algorithm/other/T167-two-sum-ii-input-array-is-sorted.c
@@ -6,4 +6,39 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+
+
 #include "T167-two-sum-ii-input-array-is-sorted.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
+    if (numbers == NULL || numbersSize <= 0) { return NULL; }
+    *returnSize = 2;
+    
+    int *result = malloc(sizeof(int) * (*returnSize));
+    for (int i = 0; i < numbersSize - 1; i++) {
+        int left = i + 1;
+        int right = numbersSize - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            int num = numbers[mid];
+            int remaining = target - numbers[i];
+            if (num < remaining) {
+                left = mid + 1;
+            } else if (num > remaining) {
+                right = mid - 1;
+            } else {
+                result[0] = i + 1;
+                result[1] = mid + 1;
+                break;
+            }
+        }
+    }
+    
+   return result;
+}
