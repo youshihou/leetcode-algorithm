db259ea21d65b8425191d784829dec94187fec89
oneday <youshihou5@gmail.com>
2020-07-20 09:45:54 +0800

    add T167-two-sum-ii-input-array-is-sorted

---
+++ algorithm/algorithm/other/T167-two-sum-ii-input-array-is-sorted.c
@@ -0,0 +1,9 @@
+//
+//  T167-two-sum-ii-input-array-is-sorted.c
+//  algorithm
+//
+//  Created by Ankui on 7/20/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T167-two-sum-ii-input-array-is-sorted.h"
---
+++ algorithm/algorithm/other/T167-two-sum-ii-input-array-is-sorted.h
@@ -0,0 +1,14 @@
+//
+//  T167-two-sum-ii-input-array-is-sorted.h
+//  algorithm
+//
+//  Created by Ankui on 7/20/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T167_two_sum_ii_input_array_is_sorted_h
+#define T167_two_sum_ii_input_array_is_sorted_h
+
+#include <stdio.h>
+
+#endif /* T167_two_sum_ii_input_array_is_sorted_h */
