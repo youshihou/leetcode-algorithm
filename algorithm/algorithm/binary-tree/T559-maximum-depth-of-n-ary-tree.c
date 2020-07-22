726000a3910d822724529f247389cf983c56666a
oneday <youshihou5@gmail.com>
2020-07-22 21:47:18 +0800

    add T559-maximum-depth-of-n-ary-tree

---
+++ algorithm/algorithm/binary-tree/T559-maximum-depth-of-n-ary-tree.c
@@ -0,0 +1,9 @@
+//
+//  T559-maximum-depth-of-n-ary-tree.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T559-maximum-depth-of-n-ary-tree.h"
---
+++ algorithm/algorithm/binary-tree/T559-maximum-depth-of-n-ary-tree.h
@@ -0,0 +1,14 @@
+//
+//  T559-maximum-depth-of-n-ary-tree.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T559_maximum_depth_of_n_ary_tree_h
+#define T559_maximum_depth_of_n_ary_tree_h
+
+#include <stdio.h>
+
+#endif /* T559_maximum_depth_of_n_ary_tree_h */
