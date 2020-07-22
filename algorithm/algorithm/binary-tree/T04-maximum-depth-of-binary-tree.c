d5423cc7b006f65db4eff001d15f75b8bd1e6516
oneday <youshihou5@gmail.com>
2020-07-22 21:34:50 +0800

    add T04-maximum-depth-of-binary-tree

--- algorithm/algorithm/binary-tree/T04-maximum-depth-of-binary-tree.c
+++ algorithm/algorithm/binary-tree/T04-maximum-depth-of-binary-tree.c
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+
+
 #include "T04-maximum-depth-of-binary-tree.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int maxDepth(struct TreeNode* root) {
+
+    return 0;
+}
