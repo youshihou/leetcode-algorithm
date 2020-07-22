b9482680a75a141a7a48c34017b230202c3e0b2c
oneday <youshihou5@gmail.com>
2020-07-22 21:45:38 +0800

    add T590-n-ary-tree-postorder-traversal

---
+++ algorithm/algorithm/binary-tree/T590-n-ary-tree-postorder-traversal.c
@@ -0,0 +1,9 @@
+//
+//  T590-n-ary-tree-postorder-traversal.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T590-n-ary-tree-postorder-traversal.h"
---
+++ algorithm/algorithm/binary-tree/T590-n-ary-tree-postorder-traversal.h
@@ -0,0 +1,14 @@
+//
+//  T590-n-ary-tree-postorder-traversal.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T590_n_ary_tree_postorder_traversal_h
+#define T590_n_ary_tree_postorder_traversal_h
+
+#include <stdio.h>
+
+#endif /* T590_n_ary_tree_postorder_traversal_h */
