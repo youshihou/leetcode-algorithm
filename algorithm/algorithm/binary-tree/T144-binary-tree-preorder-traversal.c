7c3f3390cfca21dbdec2e56ab1a348e646584918
oneday <youshihou5@gmail.com>
2020-07-22 21:21:33 +0800

    add T144-binary-tree-preorder-traversal

--- algorithm/algorithm/binary-tree/T144-binary-tree-preorder-traversal.c
+++ algorithm/algorithm/binary-tree/T144-binary-tree-preorder-traversal.c
@@ -6,4 +6,27 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+
+
 #include "T144-binary-tree-preorder-traversal.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+
+    return NULL;
+}
