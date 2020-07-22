97a16e091605656acb2d56c923ec451d2937fe89
oneday <youshihou5@gmail.com>
2020-07-22 21:39:44 +0800

    add T07-binary-tree-level-order-traversal-ii

--- algorithm/algorithm/binary-tree/T07-binary-tree-level-order-traversal-ii.c
+++ algorithm/algorithm/binary-tree/T07-binary-tree-level-order-traversal-ii.c
@@ -6,4 +6,29 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/
+
+
 #include "T07-binary-tree-level-order-traversal-ii.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** levelOrderBottom(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+
+    return NULL;
+}
