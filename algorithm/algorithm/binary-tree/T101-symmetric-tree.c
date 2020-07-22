54b2d132fc119cdb02fa91ff41d06452f9f9d981
oneday <youshihou5@gmail.com>
2020-07-22 22:07:59 +0800

    add T101-symmetric-tree

---
+++ algorithm/algorithm/binary-tree/T101-symmetric-tree.c
@@ -0,0 +1,9 @@
+//
+//  T101-symmetric-tree.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T101-symmetric-tree.h"
---
+++ algorithm/algorithm/binary-tree/T101-symmetric-tree.h
@@ -0,0 +1,14 @@
+//
+//  T101-symmetric-tree.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T101_symmetric_tree_h
+#define T101_symmetric_tree_h
+
+#include <stdio.h>
+
+#endif /* T101_symmetric_tree_h */
