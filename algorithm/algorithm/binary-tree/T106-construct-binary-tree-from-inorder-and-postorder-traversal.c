112b4d5beac6fc26e34399a488d9a1b0d8a6224f
oneday <youshihou5@gmail.com>
2020-07-22 21:53:35 +0800

    add T106-construct-binary-tree-from-inorder-and-postorder-traversal

---
+++ algorithm/algorithm/binary-tree/T106-construct-binary-tree-from-inorder-and-postorder-traversal.c
@@ -0,0 +1,9 @@
+//
+//  T106-construct-binary-tree-from-inorder-and-postorder-traversal.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T106-construct-binary-tree-from-inorder-and-postorder-traversal.h"
---
+++ algorithm/algorithm/binary-tree/T106-construct-binary-tree-from-inorder-and-postorder-traversal.h
@@ -0,0 +1,14 @@
+//
+//  T106-construct-binary-tree-from-inorder-and-postorder-traversal.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T106_construct_binary_tree_from_inorder_and_postorder_traversal_h
+#define T106_construct_binary_tree_from_inorder_and_postorder_traversal_h
+
+#include <stdio.h>
+
+#endif /* T106_construct_binary_tree_from_inorder_and_postorder_traversal_h */
