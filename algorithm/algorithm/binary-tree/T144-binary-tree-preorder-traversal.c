0948b0f10bc2e2a1cf6790164a010a4458e64eb6
oneday <youshihou5@gmail.com>
2020-07-22 21:21:27 +0800

    add T144-binary-tree-preorder-traversal

---
+++ algorithm/algorithm/binary-tree/T144-binary-tree-preorder-traversal.c
@@ -0,0 +1,9 @@
+//
+//  T144-binary-tree-preorder-traversal.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T144-binary-tree-preorder-traversal.h"
---
+++ algorithm/algorithm/binary-tree/T144-binary-tree-preorder-traversal.h
@@ -0,0 +1,14 @@
+//
+//  T144-binary-tree-preorder-traversal.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T144_binary_tree_preorder_traversal_h
+#define T144_binary_tree_preorder_traversal_h
+
+#include <stdio.h>
+
+#endif /* T144_binary_tree_preorder_traversal_h */
