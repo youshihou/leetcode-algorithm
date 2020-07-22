99883d64ee839888779c6bee2ce2c77c7a574341
oneday <youshihou5@gmail.com>
2020-07-22 21:53:40 +0800

    add T106-construct-binary-tree-from-inorder-and-postorder-traversal

--- algorithm/algorithm/binary-tree/T106-construct-binary-tree-from-inorder-and-postorder-traversal.c
+++ algorithm/algorithm/binary-tree/T106-construct-binary-tree-from-inorder-and-postorder-traversal.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+
 #include "T106-construct-binary-tree-from-inorder-and-postorder-traversal.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* buildTree(int* inorder, int inorderSize, int* postorder, int postorderSize) {
+
+    return NULL;
+}
