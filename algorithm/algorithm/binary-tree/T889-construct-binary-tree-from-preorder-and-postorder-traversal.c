a2061e27ead27f94ab2d87dfde9bcb768975f453
oneday <youshihou5@gmail.com>
2020-07-22 22:06:25 +0800

    add T889-construct-binary-tree-from-preorder-and-postorder-traversal

---
+++ algorithm/algorithm/binary-tree/T889-construct-binary-tree-from-preorder-and-postorder-traversal.c
@@ -0,0 +1,9 @@
+//
+//  T889-construct-binary-tree-from-preorder-and-postorder-traversal.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T889-construct-binary-tree-from-preorder-and-postorder-traversal.h"
---
+++ algorithm/algorithm/binary-tree/T889-construct-binary-tree-from-preorder-and-postorder-traversal.h
@@ -0,0 +1,14 @@
+//
+//  T889-construct-binary-tree-from-preorder-and-postorder-traversal.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T889_construct_binary_tree_from_preorder_and_postorder_traversal_h
+#define T889_construct_binary_tree_from_preorder_and_postorder_traversal_h
+
+#include <stdio.h>
+
+#endif /* T889_construct_binary_tree_from_preorder_and_postorder_traversal_h */
