d71dc1a5a0bf3934954e10c08a7568f67dcc433c
oneday <youshihou5@gmail.com>
2020-07-22 22:06:30 +0800

    add T889-construct-binary-tree-from-preorder-and-postorder-traversal

--- algorithm/algorithm/binary-tree/T889-construct-binary-tree-from-preorder-and-postorder-traversal.c
+++ algorithm/algorithm/binary-tree/T889-construct-binary-tree-from-preorder-and-postorder-traversal.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+
+
 #include "T889-construct-binary-tree-from-preorder-and-postorder-traversal.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* constructFromPrePost(int* pre, int preSize, int* post, int postSize) {
+
+    return NULL;
+}
