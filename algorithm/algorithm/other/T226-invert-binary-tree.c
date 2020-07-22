1494a8c505c5b793dfd32dbc53c441641f82e746
oneday <youshihou5@gmail.com>
2020-07-22 21:10:00 +0800

    add T226-invert-binary-tree

---
+++ algorithm/algorithm/other/T226-invert-binary-tree.c
@@ -0,0 +1,9 @@
+//
+//  T226-invert-binary-tree.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T226-invert-binary-tree.h"
---
+++ algorithm/algorithm/other/T226-invert-binary-tree.h
@@ -0,0 +1,14 @@
+//
+//  T226-invert-binary-tree.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T226_invert_binary_tree_h
+#define T226_invert_binary_tree_h
+
+#include <stdio.h>
+
+#endif /* T226_invert_binary_tree_h */
