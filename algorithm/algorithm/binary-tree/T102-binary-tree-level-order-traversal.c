aa3cc46ee518b3d2884cc70a9560bb327860c40a
oneday <youshihou5@gmail.com>
2020-07-22 21:26:34 +0800

    add T102-binary-tree-level-order-traversal

--- algorithm/algorithm/binary-tree/T102-binary-tree-level-order-traversal.c
+++ algorithm/algorithm/binary-tree/T102-binary-tree-level-order-traversal.c
@@ -6,4 +6,29 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+
+
 #include "T102-binary-tree-level-order-traversal.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+
+    return NULL;
+}
