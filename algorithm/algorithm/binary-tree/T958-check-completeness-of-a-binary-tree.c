c61f4b5b92838a1c4d084e0a38ae0e121c7bddb6
oneday <youshihou5@gmail.com>
2020-07-22 21:31:46 +0800

    add T958-check-completeness-of-a-binary-tree

---
+++ algorithm/algorithm/binary-tree/T958-check-completeness-of-a-binary-tree.c
@@ -0,0 +1,9 @@
+//
+//  T958-check-completeness-of-a-binary-tree.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T958-check-completeness-of-a-binary-tree.h"
---
+++ algorithm/algorithm/binary-tree/T958-check-completeness-of-a-binary-tree.h
@@ -0,0 +1,14 @@
+//
+//  T958-check-completeness-of-a-binary-tree.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T958_check_completeness_of_a_binary_tree_h
+#define T958_check_completeness_of_a_binary_tree_h
+
+#include <stdio.h>
+
+#endif /* T958_check_completeness_of_a_binary_tree_h */
