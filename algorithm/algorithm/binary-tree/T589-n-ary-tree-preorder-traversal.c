a3cc3f300be993988ebbd0c785a9f13e36dfc34d
oneday <youshihou5@gmail.com>
2020-07-22 21:43:59 +0800

    add T589-n-ary-tree-preorder-traversal

---
+++ algorithm/algorithm/binary-tree/T589-n-ary-tree-preorder-traversal.c
@@ -0,0 +1,9 @@
+//
+//  T589-n-ary-tree-preorder-traversal.c
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T589-n-ary-tree-preorder-traversal.h"
---
+++ algorithm/algorithm/binary-tree/T589-n-ary-tree-preorder-traversal.h
@@ -0,0 +1,14 @@
+//
+//  T589-n-ary-tree-preorder-traversal.h
+//  algorithm
+//
+//  Created by Ankui on 7/22/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T589_n_ary_tree_preorder_traversal_h
+#define T589_n_ary_tree_preorder_traversal_h
+
+#include <stdio.h>
+
+#endif /* T589_n_ary_tree_preorder_traversal_h */
