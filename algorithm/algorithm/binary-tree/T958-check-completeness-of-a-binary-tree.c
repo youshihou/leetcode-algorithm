f87e18aa143ef3ff472cfe9745870c8ad072a900
oneday <youshihou5@gmail.com>
2020-07-22 21:31:51 +0800

    add T958-check-completeness-of-a-binary-tree

--- algorithm/algorithm/binary-tree/T958-check-completeness-of-a-binary-tree.c
+++ algorithm/algorithm/binary-tree/T958-check-completeness-of-a-binary-tree.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/check-completeness-of-a-binary-tree/
+
+
 #include "T958-check-completeness-of-a-binary-tree.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isCompleteTree(struct TreeNode* root) {
+
+    return true;
+}
