7d1e753bec834c1b491f12de6077f8d31a39a4ba
oneday <youshihou5@gmail.com>
2020-07-22 21:22:59 +0800

    add T94-binary-tree-inorder-traversal

--- algorithm/algorithm/binary-tree/T94-binary-tree-inorder-traversal.c
+++ algorithm/algorithm/binary-tree/T94-binary-tree-inorder-traversal.c
@@ -6,4 +6,26 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+
+
 #include "T94-binary-tree-inorder-traversal.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+
+    return NULL;
+}
