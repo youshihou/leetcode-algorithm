25be1cdd3e83506f579ad2443fef64c4715794ce
oneday <youshihou5@gmail.com>
2020-07-22 21:24:21 +0800

    add T145-binary-tree-postorder-traversal

--- algorithm/algorithm/binary-tree/T145-binary-tree-postorder-traversal.c
+++ algorithm/algorithm/binary-tree/T145-binary-tree-postorder-traversal.c
@@ -6,4 +6,27 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+
+
 #include "T145-binary-tree-postorder-traversal.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+
+    return NULL;
+}
