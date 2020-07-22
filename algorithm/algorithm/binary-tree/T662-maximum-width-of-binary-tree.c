781c6185ba1bc338373c335afcda7ed0372f68ca
oneday <youshihou5@gmail.com>
2020-07-22 21:37:21 +0800

    add T662-maximum-width-of-binary-tree

--- algorithm/algorithm/binary-tree/T662-maximum-width-of-binary-tree.c
+++ algorithm/algorithm/binary-tree/T662-maximum-width-of-binary-tree.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/maximum-width-of-binary-tree/
+
+
 #include "T662-maximum-width-of-binary-tree.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int widthOfBinaryTree(struct TreeNode* root) {
+
+    return 0;
+}
