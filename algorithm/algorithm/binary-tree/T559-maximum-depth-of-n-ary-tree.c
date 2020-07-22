c67de8e8775811274d43cc59f29c868b4a453f75
oneday <youshihou5@gmail.com>
2020-07-22 21:47:21 +0800

    add T559-maximum-depth-of-n-ary-tree

--- algorithm/algorithm/binary-tree/T559-maximum-depth-of-n-ary-tree.c
+++ algorithm/algorithm/binary-tree/T559-maximum-depth-of-n-ary-tree.c
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/
+
+
 #include "T559-maximum-depth-of-n-ary-tree.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     int numChildren;
+ *     struct Node** children;
+ * };
+ */
+
+int* maxDepth(struct Node* root) {
+    return NULL;
+}
