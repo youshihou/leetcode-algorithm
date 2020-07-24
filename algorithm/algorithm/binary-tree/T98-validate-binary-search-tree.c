b1f496f7858b819670b7dbc267b2ff7aeef3cd6a
oneday <youshihou5@gmail.com>
2020-07-24 19:04:40 +0800

    add T98-validate-binary-search-tree

---
+++ algorithm/algorithm/binary-tree/T98-validate-binary-search-tree.c
@@ -0,0 +1,9 @@
+//
+//  T98-validate-binary-search-tree.c
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T98-validate-binary-search-tree.h"
