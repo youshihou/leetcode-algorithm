b043f0df048bb7f5afea4b4ab69845f9e59a4452
oneday <youshihou5@gmail.com>
2020-07-24 19:06:06 +0800

    add T783-minimum-distance-between-bst-nodes

--- algorithm/algorithm/binary-tree/T783-minimum-distance-between-bst-nodes.c
+++ algorithm/algorithm/binary-tree/T783-minimum-distance-between-bst-nodes.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
+
+
 #include "T783-minimum-distance-between-bst-nodes.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int minDiffInBST(struct TreeNode* root) {
+
+    return 0;
+}
