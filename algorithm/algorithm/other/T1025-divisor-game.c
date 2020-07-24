832de7a0b9da6f72b2a7554b6c28556bd5e94c6d
oneday <youshihou5@gmail.com>
2020-07-24 18:55:19 +0800

    T1025-divisor-game

--- algorithm/algorithm/other/T1025-divisor-game.c
+++ algorithm/algorithm/other/T1025-divisor-game.c
@@ -6,4 +6,13 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/divisor-game/
+
+
 #include "T1025-divisor-game.h"
+#include "algorithm-common.h"
+
+bool divisorGame(int N) {
+
+    return N >> 1 == 0;
+}
