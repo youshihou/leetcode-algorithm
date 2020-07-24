619437375ae0b3fa6e7c60938090871474f838ab
oneday <youshihou5@gmail.com>
2020-07-24 19:21:01 +0800

    add T173-binary-search-tree-iterator

--- algorithm/algorithm/binary-tree/T173-binary-search-tree-iterator.c
+++ algorithm/algorithm/binary-tree/T173-binary-search-tree-iterator.c
@@ -6,4 +6,53 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/binary-search-tree-iterator/
+
+
 #include "T173-binary-search-tree-iterator.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+
+typedef struct {
+
+} BSTIterator;
+
+
+BSTIterator* bSTIteratorCreate(struct TreeNode* root) {
+    return NULL;
+}
+
+/** @return the next smallest number */
+int bSTIteratorNext(BSTIterator* obj) {
+    return 0;
+}
+
+/** @return whether we have a next smallest number */
+bool bSTIteratorHasNext(BSTIterator* obj) {
+    return false;
+}
+
+void bSTIteratorFree(BSTIterator* obj) {
+
+}
+
+/**
+ * Your BSTIterator struct will be instantiated and called as such:
+ * BSTIterator* obj = bSTIteratorCreate(root);
+ * int param_1 = bSTIteratorNext(obj);
+ 
+ * bool param_2 = bSTIteratorHasNext(obj);
+ 
+ * bSTIteratorFree(obj);
+*/
