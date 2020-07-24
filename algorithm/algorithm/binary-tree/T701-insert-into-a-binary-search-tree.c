196afd25edc7d5ac0c881dcea4ecea2dd652c695
oneday <youshihou5@gmail.com>
2020-07-24 19:03:12 +0800

    add T701-insert-into-a-binary-search-tree

---
+++ algorithm/algorithm/binary-tree/T701-insert-into-a-binary-search-tree.c
@@ -0,0 +1,9 @@
+//
+//  T701-insert-into-a-binary-search-tree.c
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T701-insert-into-a-binary-search-tree.h"
---
+++ algorithm/algorithm/binary-tree/T701-insert-into-a-binary-search-tree.h
@@ -0,0 +1,14 @@
+//
+//  T701-insert-into-a-binary-search-tree.h
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T701_insert_into_a_binary_search_tree_h
+#define T701_insert_into_a_binary_search_tree_h
+
+#include <stdio.h>
+
+#endif /* T701_insert_into_a_binary_search_tree_h */
