61032f836c87b7f067922dfd8dc57b136f9cacf0
oneday <youshihou5@gmail.com>
2020-07-24 19:01:47 +0800

    add T700-search-in-a-binary-search-tree

--- algorithm/algorithm/binary-tree/T700-search-in-a-binary-search-tree.c
+++ algorithm/algorithm/binary-tree/T700-search-in-a-binary-search-tree.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/search-in-a-binary-search-tree/
+
+
 #include "T700-search-in-a-binary-search-tree.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+
+    return NULL;
+}
