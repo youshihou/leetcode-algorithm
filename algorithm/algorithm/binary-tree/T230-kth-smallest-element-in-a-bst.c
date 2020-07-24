3a7c5ffb5de88c060b7a242c35abcd2ad074cca3
oneday <youshihou5@gmail.com>
2020-07-24 19:18:54 +0800

    T230-kth-smallest-element-in-a-bst

---
+++ algorithm/algorithm/binary-tree/T230-kth-smallest-element-in-a-bst.c
@@ -0,0 +1,9 @@
+//
+//  T230-kth-smallest-element-in-a-bst.c
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T230-kth-smallest-element-in-a-bst.h"
---
+++ algorithm/algorithm/binary-tree/T230-kth-smallest-element-in-a-bst.h
@@ -0,0 +1,14 @@
+//
+//  T230-kth-smallest-element-in-a-bst.h
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T230_kth_smallest_element_in_a_bst_h
+#define T230_kth_smallest_element_in_a_bst_h
+
+#include <stdio.h>
+
+#endif /* T230_kth_smallest_element_in_a_bst_h */
