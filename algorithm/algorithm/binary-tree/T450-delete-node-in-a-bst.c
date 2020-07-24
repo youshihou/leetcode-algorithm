5e29cf6d23e83f6b17508c5b46b46b6bb71912df
oneday <youshihou5@gmail.com>
2020-07-24 18:59:29 +0800

    add T450-delete-node-in-a-bst

---
+++ algorithm/algorithm/binary-tree/T450-delete-node-in-a-bst.c
@@ -0,0 +1,9 @@
+//
+//  T450-delete-node-in-a-bst.c
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T450-delete-node-in-a-bst.h"
---
+++ algorithm/algorithm/binary-tree/T450-delete-node-in-a-bst.h
@@ -0,0 +1,14 @@
+//
+//  T450-delete-node-in-a-bst.h
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T450_delete_node_in_a_bst_h
+#define T450_delete_node_in_a_bst_h
+
+#include <stdio.h>
+
+#endif /* T450_delete_node_in_a_bst_h */
