d4f5ae5628bd3da1412d75f46db225a71e67d5e9
oneday <youshihou5@gmail.com>
2020-07-24 19:01:42 +0800

    add T700-search-in-a-binary-search-tree

---
+++ algorithm/algorithm/binary-tree/T700-search-in-a-binary-search-tree.c
@@ -0,0 +1,9 @@
+//
+//  T700-search-in-a-binary-search-tree.c
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T700-search-in-a-binary-search-tree.h"
---
+++ algorithm/algorithm/binary-tree/T700-search-in-a-binary-search-tree.h
@@ -0,0 +1,14 @@
+//
+//  T700-search-in-a-binary-search-tree.h
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T700_search_in_a_binary_search_tree_h
+#define T700_search_in_a_binary_search_tree_h
+
+#include <stdio.h>
+
+#endif /* T700_search_in_a_binary_search_tree_h */
