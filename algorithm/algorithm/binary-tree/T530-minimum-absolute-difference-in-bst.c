da41e10bbf1215c6547be84d7b5f26cce7f90aa5
oneday <youshihou5@gmail.com>
2020-07-24 19:10:08 +0800

    add T530-minimum-absolute-difference-in-bst

--- algorithm/algorithm/binary-tree/T530-minimum-absolute-difference-in-bst.c
+++ algorithm/algorithm/binary-tree/T530-minimum-absolute-difference-in-bst.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+
+
 #include "T530-minimum-absolute-difference-in-bst.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int getMinimumDifference(struct TreeNode* root) {
+
+    return 0;
+}
