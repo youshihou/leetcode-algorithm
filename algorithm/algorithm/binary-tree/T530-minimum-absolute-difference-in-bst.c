68d7ae0d828a7d48bd2a3de7b884ced9e3d122d9
oneday <youshihou5@gmail.com>
2020-07-24 19:10:04 +0800

    add T530-minimum-absolute-difference-in-bst

---
+++ algorithm/algorithm/binary-tree/T530-minimum-absolute-difference-in-bst.c
@@ -0,0 +1,9 @@
+//
+//  T530-minimum-absolute-difference-in-bst.c
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T530-minimum-absolute-difference-in-bst.h"
---
+++ algorithm/algorithm/binary-tree/T530-minimum-absolute-difference-in-bst.h
@@ -0,0 +1,14 @@
+//
+//  T530-minimum-absolute-difference-in-bst.h
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T530_minimum_absolute_difference_in_bst_h
+#define T530_minimum_absolute_difference_in_bst_h
+
+#include <stdio.h>
+
+#endif /* T530_minimum_absolute_difference_in_bst_h */
