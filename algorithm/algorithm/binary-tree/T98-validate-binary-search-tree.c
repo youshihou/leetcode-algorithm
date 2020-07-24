3d8aae1f53caf430992e8f441ce80963e29c1348
oneday <youshihou5@gmail.com>
2020-07-24 19:04:45 +0800

    add T98-validate-binary-search-tree

--- algorithm/algorithm/binary-tree/T98-validate-binary-search-tree.c
+++ algorithm/algorithm/binary-tree/T98-validate-binary-search-tree.c
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/validate-binary-search-tree/
+
+
 #include "T98-validate-binary-search-tree.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isValidBST(struct TreeNode* root) {
+    return false;
+}
