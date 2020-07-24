944e4a0280ea443764afe21ff189496f30ab2dea
oneday <youshihou5@gmail.com>
2020-07-24 18:54:27 +0800

    T1025-divisor-game

---
+++ algorithm/algorithm/other/T1025-divisor-game.c
@@ -0,0 +1,9 @@
+//
+//  T1025-divisor-game.c
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T1025-divisor-game.h"
---
+++ algorithm/algorithm/other/T1025-divisor-game.h
@@ -0,0 +1,14 @@
+//
+//  T1025-divisor-game.h
+//  algorithm
+//
+//  Created by Ankui on 7/24/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T1025_divisor_game_h
+#define T1025_divisor_game_h
+
+#include <stdio.h>
+
+#endif /* T1025_divisor_game_h */
