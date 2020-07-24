de994f6f43d5b4e94861019cf6b2ca5ba5b27418
oneday <youshihou5@gmail.com>
2020-07-24 19:03:17 +0800

    add T701-insert-into-a-binary-search-tree

--- algorithm/algorithm/binary-tree/T701-insert-into-a-binary-search-tree.c
+++ algorithm/algorithm/binary-tree/T701-insert-into-a-binary-search-tree.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
+
+
 #include "T701-insert-into-a-binary-search-tree.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+
+    return NULL;
+}
