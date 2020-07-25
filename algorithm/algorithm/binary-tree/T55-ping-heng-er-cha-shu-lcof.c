5b029785cd53fccd277746618ec257609617eb14
oneday <youshihou5@gmail.com>
2020-07-25 17:28:43 +0800

    add T55-ping-heng-er-cha-shu-lcof

---
+++ algorithm/algorithm/binary-tree/T55-ping-heng-er-cha-shu-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T55-ping-heng-er-cha-shu-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 7/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T55-ping-heng-er-cha-shu-lcof.h"
---
+++ algorithm/algorithm/binary-tree/T55-ping-heng-er-cha-shu-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T55-ping-heng-er-cha-shu-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 7/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T55_ping_heng_er_cha_shu_lcof_h
+#define T55_ping_heng_er_cha_shu_lcof_h
+
+#include <stdio.h>
+
+#endif /* T55_ping_heng_er_cha_shu_lcof_h */
