ecfe40d8dc12b3c1ebc4726c497ca793f9eefa0e
oneday <youshihou5@gmail.com>
2020-07-25 17:28:47 +0800

    add T55-ping-heng-er-cha-shu-lcof

--- algorithm/algorithm/binary-tree/T55-ping-heng-er-cha-shu-lcof.c
+++ algorithm/algorithm/binary-tree/T55-ping-heng-er-cha-shu-lcof.c
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/
+
+
 #include "T55-ping-heng-er-cha-shu-lcof.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isBalanced(struct TreeNode* root) {
+
+    return false;
+}
