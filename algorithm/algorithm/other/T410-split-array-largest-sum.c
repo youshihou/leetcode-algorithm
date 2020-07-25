d2e716d3db72405d05703e31c9495c41dc0b5f62
oneday <youshihou5@gmail.com>
2020-07-25 17:39:23 +0800

    add T410-split-array-largest-sum

---
+++ algorithm/algorithm/other/T410-split-array-largest-sum.c
@@ -0,0 +1,9 @@
+//
+//  T410-split-array-largest-sum.c
+//  algorithm
+//
+//  Created by Ankui on 7/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T410-split-array-largest-sum.h"
---
+++ algorithm/algorithm/other/T410-split-array-largest-sum.h
@@ -0,0 +1,14 @@
+//
+//  T410-split-array-largest-sum.h
+//  algorithm
+//
+//  Created by Ankui on 7/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T410_split_array_largest_sum_h
+#define T410_split_array_largest_sum_h
+
+#include <stdio.h>
+
+#endif /* T410_split_array_largest_sum_h */
