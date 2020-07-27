135cd99b7f18c0573b88bbc67dba60a467f91b90
oneday <youshihou5@gmail.com>
2020-07-27 19:37:38 +0800

    add T392-is-subsequence

---
+++ algorithm/algorithm/other/T392-is-subsequence.cpp
@@ -0,0 +1,9 @@
+//
+//  T392-is-subsequence.cpp
+//  algorithm
+//
+//  Created by Ankui on 7/27/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T392-is-subsequence.hpp"
---
+++ algorithm/algorithm/other/T392-is-subsequence.hpp
@@ -0,0 +1,14 @@
+//
+//  T392-is-subsequence.hpp
+//  algorithm
+//
+//  Created by Ankui on 7/27/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T392_is_subsequence_hpp
+#define T392_is_subsequence_hpp
+
+#include <stdio.h>
+
+#endif /* T392_is_subsequence_hpp */
