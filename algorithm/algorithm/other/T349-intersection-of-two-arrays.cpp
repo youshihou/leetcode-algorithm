c8e04a179915ad0b8c5ecb4f182a00743b9e93a9
oneday <youshihou5@gmail.com>
2020-07-29 21:28:27 +0800

    add T349-intersection-of-two-arrays

--- algorithm/algorithm/other/T349-intersection-of-two-arrays.cpp
+++ algorithm/algorithm/other/T349-intersection-of-two-arrays.cpp
@@ -7,3 +7,26 @@
 //
 
 #include "T349-intersection-of-two-arrays.hpp"
+
+#include <iostream>
+//#include <string>
+//#include <sstream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int>set;
+        unordered_set<int> nums1_set(nums1.begin(), nums1.end());
+        for (int i : nums2) {
+            if (nums1_set.count(i) == 1) {
+                set.insert(i);
+            }
+        }
+        return vector<int>(set.begin(), set.end());
+    }
+};
