3f3419c86e48a3b1d3d2dc5fd1bdfc088d2c232d
oneday <youshihou5@gmail.com>
2020-07-29 21:28:23 +0800

    add T349-intersection-of-two-arrays

---
+++ algorithm/algorithm/other/T349-intersection-of-two-arrays.cpp
@@ -0,0 +1,9 @@
+//
+//  T349-intersection-of-two-arrays.cpp
+//  algorithm
+//
+//  Created by Ankui on 7/29/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T349-intersection-of-two-arrays.hpp"
---
+++ algorithm/algorithm/other/T349-intersection-of-two-arrays.hpp
@@ -0,0 +1,14 @@
+//
+//  T349-intersection-of-two-arrays.hpp
+//  algorithm
+//
+//  Created by Ankui on 7/29/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T349_intersection_of_two_arrays_hpp
+#define T349_intersection_of_two_arrays_hpp
+
+#include <stdio.h>
+
+#endif /* T349_intersection_of_two_arrays_hpp */
