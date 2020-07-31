c2b563f5bb63d00201a0f55ab142ba5a74d624a1
oneday <youshihou5@gmail.com>
2020-07-31 09:08:38 +0800

    add T0803-magic-index-lcci

--- algorithm/algorithm/other/T0803-magic-index-lcci.cpp
+++ algorithm/algorithm/other/T0803-magic-index-lcci.cpp
@@ -6,4 +6,37 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/magic-index-lcci/
+
+
 #include "T0803-magic-index-lcci.hpp"
+#include "cplulplus-common.h"
+
+
+class Solution {
+public:
+    int _search(vector<int>& nums, int left, int right) {
+        if (left > right) { return -1; }
+        int mid = left + ((right - left) >> 1);
+        int left_num = _search(nums, left, mid - 1);
+        if (left_num != -1) {
+            return left_num;
+        } else if (nums[mid] == mid) {
+            return mid;
+        }
+        return _search(nums, mid + 1, right);
+    }
+    
+    int findMagicIndex(vector<int>& nums) {
+        return _search(nums, 0, (int)(nums.size() - 1));
+    }
+    
+    int findMagicIndex1(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == nums[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
