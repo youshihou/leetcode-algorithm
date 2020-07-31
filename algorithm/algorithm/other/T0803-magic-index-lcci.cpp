d5162186e6a38140a15c5fcc2308213b2bd417fe
oneday <youshihou5@gmail.com>
2020-07-31 09:08:34 +0800

    add T0803-magic-index-lcci

---
+++ algorithm/algorithm/other/T0803-magic-index-lcci.cpp
@@ -0,0 +1,9 @@
+//
+//  T0803-magic-index-lcci.cpp
+//  algorithm
+//
+//  Created by Ankui on 7/31/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T0803-magic-index-lcci.hpp"
---
+++ algorithm/algorithm/other/T0803-magic-index-lcci.hpp
@@ -0,0 +1,14 @@
+//
+//  T0803-magic-index-lcci.hpp
+//  algorithm
+//
+//  Created by Ankui on 7/31/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T0803_magic_index_lcci_hpp
+#define T0803_magic_index_lcci_hpp
+
+#include <stdio.h>
+
+#endif /* T0803_magic_index_lcci_hpp */
