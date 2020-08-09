d62fb3848438c17a13892559b6cd4e4068266711
oneday <youshihou5@gmail.com>
2020-08-09 22:12:49 +0800

    add T632-smallest-range-covering-elements-from-k-lists

---
+++ algorithm/algorithm/other/T632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,9 @@
+//
+//  T632-smallest-range-covering-elements-from-k-lists.cpp
+//  algorithm
+//
+//  Created by Ankui on 8/1/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "T632-smallest-range-covering-elements-from-k-lists.hpp"
---
+++ algorithm/algorithm/other/T632-smallest-range-covering-elements-from-k-lists.hpp
@@ -0,0 +1,14 @@
+//
+//  T632-smallest-range-covering-elements-from-k-lists.hpp
+//  algorithm
+//
+//  Created by Ankui on 8/1/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef T632_smallest_range_covering_elements_from_k_lists_hpp
+#define T632_smallest_range_covering_elements_from_k_lists_hpp
+
+#include <stdio.h>
+
+#endif /* T632_smallest_range_covering_elements_from_k_lists_hpp */
