bae13a940d09fe6dccdd24d06f5ab2b902dc4355
oneday <youshihou5@gmail.com>
2021-04-14 22:38:00 +0800

    add T977-squares-of-a-sorted-array

--- algorithm/algorithm/array-sort/T977-squares-of-a-sorted-array.c
+++ algorithm/algorithm/array-sort/T977-squares-of-a-sorted-array.c
@@ -12,6 +12,32 @@
 #include "T977-squares-of-a-sorted-array.h"
 #include <stdlib.h>
 
+int* sortedSquares(int* A, int ASize, int* returnSize) {
+    int* result = malloc(sizeof(int) * ASize);
+    *returnSize = ASize;
+
+    int l = 0;
+    int r = ASize - 1;
+    int cur = ASize - 1;
+    
+    while (l <= r) {
+        int vl = A[l];
+        int vr = A[r];
+        if (abs(vl) > abs(vr)) {
+            result[cur] = vl * vl;
+            cur--;
+            l++;
+        } else {
+            result[cur] = vr * vr;
+            cur--;
+            r--;
+        }
+    }
+        
+    return result;
+}
+
+
 
 int t977cmp(const void * v1, const void * v2) {
     return *(int *)v1 - *(int *)v2;
@@ -20,7 +46,7 @@ int t977cmp(const void * v1, const void * v2) {
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* sortedSquares(int* A, int ASize, int* returnSize) {
+int* sortedSquares1(int* A, int ASize, int* returnSize) {
     int* result = malloc(sizeof(int) * ASize);
     *returnSize = ASize;
     
