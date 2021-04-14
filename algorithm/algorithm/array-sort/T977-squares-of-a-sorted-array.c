6d705ff85e32ac83ee86833a661758c4257ef2f6
oneday <youshihou5@gmail.com>
2021-04-14 22:27:12 +0800

    add T977-squares-of-a-sorted-array

--- algorithm/algorithm/array-sort/T977-squares-of-a-sorted-array.c
+++ algorithm/algorithm/array-sort/T977-squares-of-a-sorted-array.c
@@ -10,11 +10,25 @@
 
 
 #include "T977-squares-of-a-sorted-array.h"
+#include <stdlib.h>
+
+
+int t977cmp(const void * v1, const void * v2) {
+    return *(int *)v1 - *(int *)v2;
+}
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* sortedSquares(int* A, int ASize, int* returnSize) {
+    int* result = malloc(sizeof(int) * ASize);
+    *returnSize = ASize;
     
-    return NULL;
+    for (int i = 0; i < ASize; i++) {
+        result[i] = A[i] * A[i];
+    }
+        
+    qsort(result, ASize, sizeof(int), t977cmp);
+    
+    return result;
 }
