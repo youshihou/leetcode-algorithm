1ba31508f73abbf8684f16459abae64172b1b1a5
oneday <youshihou5@gmail.com>
2021-04-14 23:00:02 +0800

    add T164-maximum-gap

--- algorithm/algorithm/array-sort/T164-maximum-gap.c
+++ algorithm/algorithm/array-sort/T164-maximum-gap.c
@@ -9,8 +9,22 @@
 // https://leetcode-cn.com/problems/maximum-gap/
 
 #include "T164-maximum-gap.h"
+#include <stdlib.h>
+#include <math.h>
+
+int t164cmp(const void * a, const void * b) {
+    return *(int *)a - *(int *)b;
+}
 
 int maximumGap(int* nums, int numsSize) {
+    if (numsSize < 2) { return 0; }
     
-    return 0;
+    qsort(nums, numsSize, sizeof(int), t164cmp);
+    
+    int result = 0;
+    for (int i = 1; i < numsSize; i++) {
+        result = fmax(result, nums[i] - nums[i - 1]);
+    }
+    
+    return result;
 }
