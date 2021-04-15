62dc761d9957fcd2303047eead1c07dc714e9f80
oneday <youshihou5@gmail.com>
2021-04-15 15:55:48 +0800

    update T92-reverse-linked-list-ii

--- algorithm/algorithm/linked-list/T92-reverse-linked-list-ii.c
+++ algorithm/algorithm/linked-list/T92-reverse-linked-list-ii.c
@@ -1,5 +1,5 @@
 //
-//  T92-reverse-linked-list-ii.cpp
+//  T92-reverse-linked-list-ii.c
 //  algorithm
 //
 //  Created by Ankui on 8/28/20.
@@ -9,7 +9,7 @@
 // https://leetcode-cn.com/problems/reverse-linked-list-ii/
 
 
-#include "T92-reverse-linked-list-ii.hpp"
+#include "T92-reverse-linked-list-ii.h"
 #include "algorithm-common.h"
 
 /**
@@ -20,23 +20,33 @@
  * };
  */
 
-struct ListNode * reverseList(struct ListNode *head) {
-    if (head == NULL || head->next == NULL) { return NULL; }
-    struct ListNode *newHead = NULL;
-    struct ListNode *tmp = NULL;
-    while (head) {
-        tmp = head->next;
-        head->next = newHead;
-        newHead = head;
-        head = tmp;
+struct ListNode * reverseList(struct ListNode *head, struct ListNode *tail) {
+    if (head == tail || head->next == tail) { return head; }
+    struct ListNode *l = NULL;
+    struct ListNode *r = tail;
+    while (head != tail) {
+        l = head->next;
+        head->next = r;
+        r = head;
+        head = l;
     }
-    return newHead;
+    return r;
 }
 
 
 struct ListNode* reverseBetween(struct ListNode* head, int m, int n) {
     struct ListNode *newHead = malloc(sizeof(struct ListNode));
-    struct ListNode *
-    
+    newHead->next = head;
+    struct ListNode *cur = newHead;
+    struct ListNode *l = NULL;
+    struct ListNode *r = NULL;
+    for (int i = 0; i <= n; i++) {
+        if (i == m - 1) {
+            l = cur;
+        }
+        cur = cur->next;
+    }
+    r = cur;
+    l->next = reverseList(l->next, r);
     return newHead->next;
 }
--- algorithm/algorithm/linked-list/T92-reverse-linked-list-ii.h
+++ algorithm/algorithm/linked-list/T92-reverse-linked-list-ii.h
@@ -1,14 +1,14 @@
 //
-//  T92-reverse-linked-list-ii.hpp
+//  T92-reverse-linked-list-ii.h
 //  algorithm
 //
 //  Created by Ankui on 8/28/20.
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
-#ifndef T92_reverse_linked_list_ii_hpp
-#define T92_reverse_linked_list_ii_hpp
+#ifndef T92_reverse_linked_list_ii_h
+#define T92_reverse_linked_list_ii_h
 
 #include <stdio.h>
 
-#endif /* T92_reverse_linked_list_ii_hpp */
+#endif /* T92_reverse_linked_list_ii_h */
