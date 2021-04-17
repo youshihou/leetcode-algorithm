65409e28cfc549a1a74f5073ca15ca13cf3eb650
oneday <youshihou5@gmail.com>
2021-04-17 22:36:04 +0800

    add T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof

--- algorithm/algorithm/offer/T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof.c
+++ algorithm/algorithm/offer/T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof.c
@@ -6,4 +6,57 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+
+
 #include "T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof.h"
+#include "algorithm-common.h"
+
+
+bool binary_search(int* nums, int size, int target) {
+    if (nums == NULL || size <= 0) { return false; }
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return true;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+bool findNumberIn2DArray(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    if (matrix == NULL || matrixSize <= 0 || *matrixColSize <= 0) { return false; }
+    for (int i = 0; i < matrixSize; i++) {
+        if (binary_search(matrix[i], matrixColSize[i], target)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+bool findNumberIn2DArray1(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    if (matrix == NULL || matrixSize <= 0 || *matrixColSize <= 0) { return false; }
+    
+    int i = 0;
+    int j = *matrixColSize - 1;
+    while (i < matrixSize && j >= 0) {
+        int cur = matrix[i][j];
+        if (cur == target) {
+            return true;
+        } else if (cur < target) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+    
+    return false;
+}
