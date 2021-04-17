7389a57130669c5f0e073ee03476663d8dda832e
oneday <youshihou5@gmail.com>
2021-04-17 22:58:33 +0800

    add T05_ti-huan-kong-ge-lcof

--- algorithm/algorithm/offer/T05_ti-huan-kong-ge-lcof.c
+++ algorithm/algorithm/offer/T05_ti-huan-kong-ge-lcof.c
@@ -6,4 +6,28 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+
+
 #include "T05_ti-huan-kong-ge-lcof.h"
+#include "algorithm-common.h"
+
+
+char* replaceSpace(char* s) {
+    int size = sizeof(char) * strlen(s) * 3 + 1;
+    char* result = malloc(size);
+    memset(result, '\0', size);
+    int i = 0;
+    int j = 0;
+    while (s[i] != '\0') {
+        if (s[i] != ' ') {
+            result[j++] = s[i];
+        } else {
+            result[j++] = '%';
+            result[j++] = '2';
+            result[j++] = '0';
+        }
+        i++;
+    }
+    return result;
+}
