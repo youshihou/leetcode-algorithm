4070aaf4d0c4e2cdbd53c9bda8bd7285ccf0c0cf
oneday <youshihou5@gmail.com>
2021-04-17 22:35:31 +0800

    add T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof

---
+++ algorithm/algorithm/offer/T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/17/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof.h"
---
+++ algorithm/algorithm/offer/T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T04_er-wei-shu-zu-zhong-de-cha-zhao-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/17/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T04_er_wei_shu_zu_zhong_de_cha_zhao_lcof_h
+#define T04_er_wei_shu_zu_zhong_de_cha_zhao_lcof_h
+
+#include <stdio.h>
+
+#endif /* T04_er_wei_shu_zu_zhong_de_cha_zhao_lcof_h */
