26c8b9e3275116f31572080edf457a5a24e841a2
oneday <youshihou5@gmail.com>
2021-04-17 16:04:10 +0800

    update T674-longest-continuous-increasing-subsequence

--- algorithm/algorithm/dynamic-programming/T674-longest-continuous-increasing-subsequence.c
+++ algorithm/algorithm/dynamic-programming/T674-longest-continuous-increasing-subsequence.c
@@ -9,8 +9,19 @@
 // https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence/
 
 #include "T674-longest-continuous-increasing-subsequence.h"
+#include "algorithm-common.h"
 
 int findLengthOfLCIS(int* nums, int numsSize) {
-
-    return 0;
+    if (nums == NULL || numsSize <= 0) { return 0; }
+    
+    int maxLen = 0;
+    int start = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (i > 0 && nums[i] <= nums[i-1]) {
+            start = i;
+        }
+        maxLen = fmax(maxLen, i - start + 1);
+    }
+    
+    return maxLen;
 }
