2bbb098e77ce51902938137f567b85f86011da43
oneday <youshihou5@gmail.com>
2021-04-17 22:58:19 +0800

    add T05_ti-huan-kong-ge-lcof

---
+++ algorithm/algorithm/offer/T05_ti-huan-kong-ge-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T05_ti-huan-kong-ge-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/17/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T05_ti-huan-kong-ge-lcof.h"
---
+++ algorithm/algorithm/offer/T05_ti-huan-kong-ge-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T05_ti-huan-kong-ge-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/17/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T05_ti_huan_kong_ge_lcof_h
+#define T05_ti_huan_kong_ge_lcof_h
+
+#include <stdio.h>
+
+#endif /* T05_ti_huan_kong_ge_lcof_h */
