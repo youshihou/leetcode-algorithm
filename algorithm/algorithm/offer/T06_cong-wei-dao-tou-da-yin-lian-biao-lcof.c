21106437667c5153cb65cf1cd11f44ef5d700d28
oneday <youshihou5@gmail.com>
2021-04-18 14:35:41 +0800

    add T06_cong-wei-dao-tou-da-yin-lian-biao-lcof

---
+++ algorithm/algorithm/offer/T06_cong-wei-dao-tou-da-yin-lian-biao-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T06_cong-wei-dao-tou-da-yin-lian-biao-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T06_cong-wei-dao-tou-da-yin-lian-biao-lcof.h"
---
+++ algorithm/algorithm/offer/T06_cong-wei-dao-tou-da-yin-lian-biao-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T06_cong-wei-dao-tou-da-yin-lian-biao-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T06_cong_wei_dao_tou_da_yin_lian_biao_lcof_h
+#define T06_cong_wei_dao_tou_da_yin_lian_biao_lcof_h
+
+#include <stdio.h>
+
+#endif /* T06_cong_wei_dao_tou_da_yin_lian_biao_lcof_h */
