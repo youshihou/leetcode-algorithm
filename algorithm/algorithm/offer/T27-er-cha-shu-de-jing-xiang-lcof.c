0eba947d76617a3627937acbefc367aac58c660c
oneday <youshihou5@gmail.com>
2021-04-18 21:29:10 +0800

    add T27-er-cha-shu-de-jing-xiang-lcof

--- algorithm/algorithm/offer/T27-er-cha-shu-de-jing-xiang-lcof.c
+++ algorithm/algorithm/offer/T27-er-cha-shu-de-jing-xiang-lcof.c
@@ -6,4 +6,39 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
+
+
 #include "T27-er-cha-shu-de-jing-xiang-lcof.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* mirrorTree(struct TreeNode* root) {
+    if (root == NULL) { return root; }
+    
+    struct TreeNode* tmp = root->left;
+    root->left = root->right;
+    root->right = tmp;
+    mirrorTree(root->left);
+    mirrorTree(root->right);
+    return root;
+}
+
+struct TreeNode* mirrorTree1(struct TreeNode* root) {
+    if (root == NULL) { return root; }
+    
+    struct TreeNode* tmp = root->left;
+    root->left = mirrorTree(root->right);
+    root->right = mirrorTree(tmp);
+    return root;
+}
