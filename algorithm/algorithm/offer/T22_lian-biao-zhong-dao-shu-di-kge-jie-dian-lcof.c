b16296f7c48e5cb2bcb7480f7746894fcb4c6fa7
oneday <youshihou5@gmail.com>
2021-04-18 20:38:05 +0800

    add T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof

--- algorithm/algorithm/offer/T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.c
+++ algorithm/algorithm/offer/T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.c
@@ -6,4 +6,33 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+
+
 #include "T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* getKthFromEnd(struct ListNode* head, int k) {
+    if (head == NULL || k <= 0) { return head; }
+    
+    struct ListNode* fast = head;
+    struct ListNode* slow = head;
+    while (k--) {
+        fast = fast->next;
+    }
+    
+    while (fast) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return slow;
+}
