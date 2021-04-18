272f4e27071454962d377e3acc6175103267a83d
oneday <youshihou5@gmail.com>
2021-04-18 15:23:02 +0800

    add T10-I_fei-bo-na-qi-shu-lie-lcof

---
+++ algorithm/algorithm/offer/T10-I_fei-bo-na-qi-shu-lie-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T10-I_fei-bo-na-qi-shu-lie-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T10-I_fei-bo-na-qi-shu-lie-lcof.h"
---
+++ algorithm/algorithm/offer/T10-I_fei-bo-na-qi-shu-lie-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T10-I_fei-bo-na-qi-shu-lie-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T10_I_fei_bo_na_qi_shu_lie_lcof_h
+#define T10_I_fei_bo_na_qi_shu_lie_lcof_h
+
+#include <stdio.h>
+
+#endif /* T10_I_fei_bo_na_qi_shu_lie_lcof_h */
