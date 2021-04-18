d79f0838dfb44e8d815b2ef3c36dd2efc14caa7f
oneday <youshihou5@gmail.com>
2021-04-18 21:10:33 +0800

    add T25-he-bing-liang-ge-pai-xu-de-lian-biao-lcof

--- algorithm/algorithm/offer/T25-he-bing-liang-ge-pai-xu-de-lian-biao-lcof.c
+++ algorithm/algorithm/offer/T25-he-bing-liang-ge-pai-xu-de-lian-biao-lcof.c
@@ -24,6 +24,18 @@
 
 struct ListNode* __mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
     if (l1 == NULL || l2 == NULL) { return NULL; }
-    
-    return NULL;
+    struct ListNode* newHead = malloc(sizeof(struct ListNode));
+    struct ListNode* cur = newHead;
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            cur->next = l1;
+            l1 = l1->next;
+        } else {
+            cur->next = l2;
+            l2 = l2->next;
+        }
+        cur = cur->next;
+    }
+    cur->next = l1 ?: l2;
+    return newHead->next;
 }
