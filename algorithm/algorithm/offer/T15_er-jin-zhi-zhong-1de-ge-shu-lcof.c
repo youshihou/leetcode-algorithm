e089f7166be2824d242b7d65d38a9763d9d863a4
oneday <youshihou5@gmail.com>
2021-04-18 17:20:36 +0800

    add T15_er-jin-zhi-zhong-1de-ge-shu-lcof

--- algorithm/algorithm/offer/T15_er-jin-zhi-zhong-1de-ge-shu-lcof.c
+++ algorithm/algorithm/offer/T15_er-jin-zhi-zhong-1de-ge-shu-lcof.c
@@ -6,4 +6,17 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/
+
+
 #include "T15_er-jin-zhi-zhong-1de-ge-shu-lcof.h"
+#include "algorithm-common.h"
+
+int hammingWeight(uint32_t n) {
+    int result = 0;
+    while (n != 0) {
+        n &= (n - 1);
+        result++;
+    }
+    return result;
+}
