6fb28eaf898f964eb4f8ba8b1ffac1ab96b50b93
oneday <youshihou5@gmail.com>
2021-04-18 21:29:00 +0800

    add T27-er-cha-shu-de-jing-xiang-lcof

---
+++ algorithm/algorithm/offer/T27-er-cha-shu-de-jing-xiang-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T27-er-cha-shu-de-jing-xiang-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T27-er-cha-shu-de-jing-xiang-lcof.h"
---
+++ algorithm/algorithm/offer/T27-er-cha-shu-de-jing-xiang-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T27-er-cha-shu-de-jing-xiang-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T27_er_cha_shu_de_jing_xiang_lcof_h
+#define T27_er_cha_shu_de_jing_xiang_lcof_h
+
+#include <stdio.h>
+
+#endif /* T27_er_cha_shu_de_jing_xiang_lcof_h */
