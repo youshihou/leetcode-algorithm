12969553b34fdad02acba36cf987c6bd577733c0
oneday <youshihou5@gmail.com>
2021-04-18 20:51:53 +0800

    add T24-fan-zhuan-lian-biao-lcof

--- algorithm/algorithm/offer/T24-fan-zhuan-lian-biao-lcof.c
+++ algorithm/algorithm/offer/T24-fan-zhuan-lian-biao-lcof.c
@@ -6,4 +6,39 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+
+
 #include "T24-fan-zhuan-lian-biao-lcof.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* reverseList(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) { return head; }
+    struct ListNode* newHead = NULL;
+    struct ListNode* tmp = NULL;
+    while (head) {
+        tmp = head->next;
+        head->next = newHead;
+        newHead = head;
+        head = tmp;
+    }
+    return newHead;
+}
+
+
+struct ListNode* reverseList1(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) { return head; }
+    struct ListNode* newHead = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return newHead;
+}
