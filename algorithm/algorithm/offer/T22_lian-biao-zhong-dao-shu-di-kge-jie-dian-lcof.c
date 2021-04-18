4835ce0f99718ee1b1bdec0646331ce9bfe34541
oneday <youshihou5@gmail.com>
2021-04-18 20:37:53 +0800

    add T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof

---
+++ algorithm/algorithm/offer/T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.h"
---
+++ algorithm/algorithm/offer/T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T22_lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof_h
+#define T22_lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof_h
+
+#include <stdio.h>
+
+#endif /* T22_lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof_h */
