302d3dd53849bfb23f24eadccc054aea8a6d477e
oneday <youshihou5@gmail.com>
2021-04-18 15:36:29 +0800

    add T10-II_qing-wa-tiao-tai-jie-wen-ti-lcof

---
+++ algorithm/algorithm/offer/T10-II_qing-wa-tiao-tai-jie-wen-ti-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T10-II_qing-wa-tiao-tai-jie-wen-ti-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T10-II_qing-wa-tiao-tai-jie-wen-ti-lcof.h"
---
+++ algorithm/algorithm/offer/T10-II_qing-wa-tiao-tai-jie-wen-ti-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T10-II_qing-wa-tiao-tai-jie-wen-ti-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T10_II_qing_wa_tiao_tai_jie_wen_ti_lcof_h
+#define T10_II_qing_wa_tiao_tai_jie_wen_ti_lcof_h
+
+#include <stdio.h>
+
+#endif /* T10_II_qing_wa_tiao_tai_jie_wen_ti_lcof_h */
