15ce126f4a398098dc553155cfb0c3cc1a7c4db7
oneday <youshihou5@gmail.com>
2021-04-18 17:20:26 +0800

    add T15_er-jin-zhi-zhong-1de-ge-shu-lcof

---
+++ algorithm/algorithm/offer/T15_er-jin-zhi-zhong-1de-ge-shu-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T15_er-jin-zhi-zhong-1de-ge-shu-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T15_er-jin-zhi-zhong-1de-ge-shu-lcof.h"
---
+++ algorithm/algorithm/offer/T15_er-jin-zhi-zhong-1de-ge-shu-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T15_er-jin-zhi-zhong-1de-ge-shu-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T15_er_jin_zhi_zhong_1de_ge_shu_lcof_h
+#define T15_er_jin_zhi_zhong_1de_ge_shu_lcof_h
+
+#include <stdio.h>
+
+#endif /* T15_er_jin_zhi_zhong_1de_ge_shu_lcof_h */
