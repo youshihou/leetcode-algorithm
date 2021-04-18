cdb84c85b53fcbf8a3f3043d9bc43e69af621fd9
oneday <youshihou5@gmail.com>
2021-04-18 20:26:15 +0800

    add T21_diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof

---
+++ algorithm/algorithm/offer/T21_diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T21_diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T21_diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.h"
---
+++ algorithm/algorithm/offer/T21_diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T21_diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/18/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T21_diao_zheng_shu_zu_shun_xu_shi_qi_shu_wei_yu_ou_shu_qian_mian_lcof_h
+#define T21_diao_zheng_shu_zu_shun_xu_shi_qi_shu_wei_yu_ou_shu_qian_mian_lcof_h
+
+#include <stdio.h>
+
+#endif /* T21_diao_zheng_shu_zu_shun_xu_shi_qi_shu_wei_yu_ou_shu_qian_mian_lcof_h */
