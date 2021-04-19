555b6695fd37ddb376ee1e76fd1b327a0078eaa6
oneday <youshihou5@gmail.com>
2021-04-19 15:13:58 +0800

    add T54-er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof

--- algorithm/algorithm/offer/T54-er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.c
+++ algorithm/algorithm/offer/T54-er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.c
@@ -6,4 +6,41 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+
+
 #include "T54-er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.h"
+#include "algorithm-common.h"
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int getCount(struct TreeNode* root) {
+    if (root == NULL) { return 0; }
+    int leftCount = getCount(root->left);
+    int rightCount = getCount(root->right);
+    return leftCount + rightCount + 1;
+}
+
+void t54dfs(struct TreeNode* root, int* list, int* count) {
+    if (root == NULL) { return; }
+    t54dfs(root->left, list, count);
+    list[(*count)++] = root->val;
+    t54dfs(root->right, list, count);
+}
+
+int kthLargest(struct TreeNode* root, int k) {
+    int size = getCount(root);
+    // int* list = malloc(sizeof(int) * size);
+    int list[size];
+    int count = 0;
+    t54dfs(root, list, &count);
+    return list[size - k];
+}
