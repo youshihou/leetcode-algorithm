819a7f2c53fadc913a7994aa3aec3814ff2982f3
oneday <youshihou5@gmail.com>
2021-04-19 22:17:26 +0800

    add T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof

--- algorithm/algorithm/offer/T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof.c
+++ algorithm/algorithm/offer/T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof.c
@@ -6,4 +6,18 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/
+
+
 #include "T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof.h"
+#include "algorithm-common.h"
+
+
+int add(int a, int b) {
+    while (b) {
+        int c = (unsigned int)(a & b) << 1;
+        a ^= b;
+        b = c;
+    }
+    return a;
+}
