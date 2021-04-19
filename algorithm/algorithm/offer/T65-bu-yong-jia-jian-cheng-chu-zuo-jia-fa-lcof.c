a863ee5571035d736ff7ea7bc67529438437254c
oneday <youshihou5@gmail.com>
2021-04-19 22:17:17 +0800

    add T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof

---
+++ algorithm/algorithm/offer/T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof.h"
---
+++ algorithm/algorithm/offer/T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T65-bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T65_bu_yong_jia_jian_cheng_chu_zuo_jia_fa_lcof_h
+#define T65_bu_yong_jia_jian_cheng_chu_zuo_jia_fa_lcof_h
+
+#include <stdio.h>
+
+#endif /* T65_bu_yong_jia_jian_cheng_chu_zuo_jia_fa_lcof_h */
