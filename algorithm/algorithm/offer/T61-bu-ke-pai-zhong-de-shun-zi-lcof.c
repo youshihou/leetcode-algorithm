164f3d7360c2b75b7ad034807dd492b6462ed483
oneday <youshihou5@gmail.com>
2021-04-19 21:05:57 +0800

    add T61-bu-ke-pai-zhong-de-shun-zi-lcof

---
+++ algorithm/algorithm/offer/T61-bu-ke-pai-zhong-de-shun-zi-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T61-bu-ke-pai-zhong-de-shun-zi-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T61-bu-ke-pai-zhong-de-shun-zi-lcof.h"
---
+++ algorithm/algorithm/offer/T61-bu-ke-pai-zhong-de-shun-zi-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T61-bu-ke-pai-zhong-de-shun-zi-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T61_bu_ke_pai_zhong_de_shun_zi_lcof_h
+#define T61_bu_ke_pai_zhong_de_shun_zi_lcof_h
+
+#include <stdio.h>
+
+#endif /* T61_bu_ke_pai_zhong_de_shun_zi_lcof_h */
