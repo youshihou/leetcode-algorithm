ebec9a1524318ad1c8c8b3477e5e8b3a9cacd2f4
oneday <youshihou5@gmail.com>
2021-04-19 16:40:10 +0800

    add T57-I_he-wei-sde-liang-ge-shu-zi-lcof

---
+++ algorithm/algorithm/offer/T57-I_he-wei-sde-liang-ge-shu-zi-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T57-I_he-wei-sde-liang-ge-shu-zi-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T57-I_he-wei-sde-liang-ge-shu-zi-lcof.h"
---
+++ algorithm/algorithm/offer/T57-I_he-wei-sde-liang-ge-shu-zi-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T57-I_he-wei-sde-liang-ge-shu-zi-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T57_I_he_wei_sde_liang_ge_shu_zi_lcof_h
+#define T57_I_he_wei_sde_liang_ge_shu_zi_lcof_h
+
+#include <stdio.h>
+
+#endif /* T57_I_he_wei_sde_liang_ge_shu_zi_lcof_h */
