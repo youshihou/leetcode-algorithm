544ffd982c5a4e3f4470e9eeaaea83510e613abd
oneday <youshihou5@gmail.com>
2021-04-19 12:33:41 +0800

    add T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof

---
+++ algorithm/algorithm/offer/T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.h"
---
+++ algorithm/algorithm/offer/T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T50_di_yi_ge_zhi_chu_xian_yi_ci_de_zi_fu_lcof_h
+#define T50_di_yi_ge_zhi_chu_xian_yi_ci_de_zi_fu_lcof_h
+
+#include <stdio.h>
+
+#endif /* T50_di_yi_ge_zhi_chu_xian_yi_ci_de_zi_fu_lcof_h */
