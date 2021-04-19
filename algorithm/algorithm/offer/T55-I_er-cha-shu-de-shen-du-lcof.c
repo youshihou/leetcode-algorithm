7035de94f1746e8184beb322c77eb9b9c0dd1ed2
oneday <youshihou5@gmail.com>
2021-04-19 15:25:37 +0800

    add T55-I_er-cha-shu-de-shen-du-lcof

--- algorithm/algorithm/offer/T55-I_er-cha-shu-de-shen-du-lcof.c
+++ algorithm/algorithm/offer/T55-I_er-cha-shu-de-shen-du-lcof.c
@@ -6,4 +6,25 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+
+
 #include "T55-I_er-cha-shu-de-shen-du-lcof.h"
+#include "algorithm-common.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) { return 0; }
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return fmax(left, right) + 1;
+}
