8eb56808b6b602b6476cd9fd586a7ebcbf878f4c
oneday <youshihou5@gmail.com>
2021-04-19 12:58:44 +0800

    add T52-liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof

---
+++ algorithm/algorithm/offer/T52-liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T52-liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T52-liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof.h"
---
+++ algorithm/algorithm/offer/T52-liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T52-liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T52_liang_ge_lian_biao_de_di_yi_ge_gong_gong_jie_dian_lcof_h
+#define T52_liang_ge_lian_biao_de_di_yi_ge_gong_gong_jie_dian_lcof_h
+
+#include <stdio.h>
+
+#endif /* T52_liang_ge_lian_biao_de_di_yi_ge_gong_gong_jie_dian_lcof_h */
