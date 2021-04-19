89aeca547ffc1db28b9281e203b970b511c135d8
oneday <youshihou5@gmail.com>
2021-04-19 09:41:11 +0800

    add T28-dui-cheng-de-er-cha-shu-lcof

---
+++ algorithm/algorithm/offer/T28-dui-cheng-de-er-cha-shu-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T28-dui-cheng-de-er-cha-shu-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T28-dui-cheng-de-er-cha-shu-lcof.h"
---
+++ algorithm/algorithm/offer/T28-dui-cheng-de-er-cha-shu-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T28-dui-cheng-de-er-cha-shu-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T28_dui_cheng_de_er_cha_shu_lcof_h
+#define T28_dui_cheng_de_er_cha_shu_lcof_h
+
+#include <stdio.h>
+
+#endif /* T28_dui_cheng_de_er_cha_shu_lcof_h */
