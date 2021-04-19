ab2c4374e747d732aa1e935f7dee70f871af13b5
oneday <youshihou5@gmail.com>
2021-04-19 11:10:35 +0800

    add T40-zui-xiao-de-kge-shu-lcof

---
+++ algorithm/algorithm/offer/T40-zui-xiao-de-kge-shu-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T40-zui-xiao-de-kge-shu-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T40-zui-xiao-de-kge-shu-lcof.h"
---
+++ algorithm/algorithm/offer/T40-zui-xiao-de-kge-shu-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T40-zui-xiao-de-kge-shu-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/19/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T40_zui_xiao_de_kge_shu_lcof_h
+#define T40_zui_xiao_de_kge_shu_lcof_h
+
+#include <stdio.h>
+
+#endif /* T40_zui_xiao_de_kge_shu_lcof_h */
