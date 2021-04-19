8f4c6108143bbe744c68bd1b039730f409f4a909
oneday <youshihou5@gmail.com>
2021-04-19 11:10:45 +0800

    add T40-zui-xiao-de-kge-shu-lcof

--- algorithm/algorithm/offer/T40-zui-xiao-de-kge-shu-lcof.c
+++ algorithm/algorithm/offer/T40-zui-xiao-de-kge-shu-lcof.c
@@ -6,4 +6,28 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+
+
 #include "T40-zui-xiao-de-kge-shu-lcof.h"
+#include "algorithm-common.h"
+
+
+int t40cmp(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getLeastNumbers(int* arr, int arrSize, int k, int* returnSize) {
+    if (arr == NULL || arrSize <= 0) { return NULL; }
+    qsort(arr, arrSize, sizeof(int), t40cmp);
+    
+    *returnSize = k;
+    int* list = malloc(sizeof(int) * k);
+    for (int i = 0; i < k; i++) {
+        list[i] = arr[i];
+    }
+    return list;
+}
