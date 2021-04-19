54f14fa88a3c0d29b89b081fe2f685ff6aee8f8c
oneday <youshihou5@gmail.com>
2021-04-19 12:33:51 +0800

    add T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof

--- algorithm/algorithm/offer/T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.c
+++ algorithm/algorithm/offer/T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.c
@@ -6,4 +6,24 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/
+
+
 #include "T50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.h"
+#include "algorithm-common.h"
+
+
+char firstUniqChar(char* s){
+    int list[26] = {0};
+    int len = (int)strlen(s);
+    for (int i = 0; i < len; i++) {
+        list[s[i] - 'a']++;
+    }
+    for (int i = 0; i < len; i++) {
+        if (list[s[i] - 'a'] == 1) {
+            return s[i];
+        }
+    }
+            
+    return ' ';
+}
