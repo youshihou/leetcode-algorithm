b35e403056f6f4c13aac247c18dc83ef08b62c71
oneday <youshihou5@gmail.com>
2021-04-19 11:20:52 +0800

    add T42-lian-xu-zi-shu-zu-de-zui-da-he-lcof

--- algorithm/algorithm/offer/T42-lian-xu-zi-shu-zu-de-zui-da-he-lcof.c
+++ algorithm/algorithm/offer/T42-lian-xu-zi-shu-zu-de-zui-da-he-lcof.c
@@ -6,4 +6,18 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+
+
 #include "T42-lian-xu-zi-shu-zu-de-zui-da-he-lcof.h"
+#include "algorithm-common.h"
+
+
+int maxSubArray(int* nums, int numsSize) {
+    int result = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        nums[i] += fmax(nums[i-1], 0);
+        result = fmax(result, nums[i]);
+    }
+    return result;
+}
