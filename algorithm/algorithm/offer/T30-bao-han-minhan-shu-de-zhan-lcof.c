deafb7ed553f0136eec5546bf05ad1f0ba3bc3eb
oneday <youshihou5@gmail.com>
2021-04-20 11:19:58 +0800

    add T30-bao-han-minhan-shu-de-zhan-lcof

---
+++ algorithm/algorithm/offer/T30-bao-han-minhan-shu-de-zhan-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T30-bao-han-minhan-shu-de-zhan-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/20/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T30-bao-han-minhan-shu-de-zhan-lcof.h"
---
+++ algorithm/algorithm/offer/T30-bao-han-minhan-shu-de-zhan-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T30-bao-han-minhan-shu-de-zhan-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/20/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T30_bao_han_minhan_shu_de_zhan_lcof_h
+#define T30_bao_han_minhan_shu_de_zhan_lcof_h
+
+#include <stdio.h>
+
+#endif /* T30_bao_han_minhan_shu_de_zhan_lcof_h */
