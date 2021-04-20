52edab8de612dd4a31c82881a97882b3d2833308
oneday <youshihou5@gmail.com>
2021-04-20 09:14:54 +0800

    add T68-er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof

--- algorithm/algorithm/offer/T68-II_er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.c
+++ algorithm/algorithm/offer/T68-II_er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.c
@@ -1,5 +1,5 @@
 //
-//  T68-er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.c
+//  T68-II_er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.c
 //  algorithm
 //
 //  Created by Ankui on 5/2/20.
@@ -8,7 +8,7 @@
 
 // https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/
 
-#include "T68-er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.h"
+#include "T68-II_er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.h"
 #include "algorithm-common.h"
 
 /**
@@ -22,6 +22,9 @@
 
 
 struct TreeNode* lowestCommonAncestor1(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
-
-    return NULL;
+    if (root == NULL || p == root || q == root) { return root; }
+    struct TreeNode* left = lowestCommonAncestor1(root->left, p, q);
+    struct TreeNode* right = lowestCommonAncestor1(root->right, p, q);
+    if (left && right) { return root; }
+    return left ?: right;
 }
--- algorithm/algorithm/offer/T68-II_er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.h
+++ algorithm/algorithm/offer/T68-II_er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.h
@@ -1,14 +1,14 @@
 //
-//  T68-er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.h
+//  T68-II_er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof.h
 //  algorithm
 //
 //  Created by Ankui on 5/2/20.
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
-#ifndef T68_er_cha_shu_de_zui_jin_gong_gong_zu_xian_lcof_h
-#define T68_er_cha_shu_de_zui_jin_gong_gong_zu_xian_lcof_h
+#ifndef T68_II_er_cha_shu_de_zui_jin_gong_gong_zu_xian_lcof_h
+#define T68_II_er_cha_shu_de_zui_jin_gong_gong_zu_xian_lcof_h
 
 #include <stdio.h>
 
-#endif /* T68_er_cha_shu_de_zui_jin_gong_gong_zu_xian_lcof_h */
+#endif /* T68_II_er_cha_shu_de_zui_jin_gong_gong_zu_xian_lcof_h */
