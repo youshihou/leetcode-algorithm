05efc02499e67424f04ae699b892936852ec1cea
oneday <youshihou5@gmail.com>
2021-04-21 12:47:26 +0800

    add T9-remove-nth-node-from-end-of-list

---
+++ algorithm/algorithm/linked-list/T9-remove-nth-node-from-end-of-list.c
@@ -0,0 +1,9 @@
+//
+//  T9-remove-nth-node-from-end-of-list.c
+//  algorithm
+//
+//  Created by Ankui on 4/21/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T9-remove-nth-node-from-end-of-list.h"
---
+++ algorithm/algorithm/linked-list/T9-remove-nth-node-from-end-of-list.h
@@ -0,0 +1,14 @@
+//
+//  T9-remove-nth-node-from-end-of-list.h
+//  algorithm
+//
+//  Created by Ankui on 4/21/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T9_remove_nth_node_from_end_of_list_h
+#define T9_remove_nth_node_from_end_of_list_h
+
+#include <stdio.h>
+
+#endif /* T9_remove_nth_node_from_end_of_list_h */
