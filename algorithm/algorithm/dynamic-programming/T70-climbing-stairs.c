fc987d58ce2de1be093ff80fc5dc832d6bd6b4cb
oneday <youshihou5@gmail.com>
2021-04-23 10:30:48 +0800

    add T70-climbing-stairs

---
+++ algorithm/algorithm/dynamic-programming/T70-climbing-stairs.c
@@ -0,0 +1,9 @@
+//
+//  T70-climbing-stairs.c
+//  algorithm
+//
+//  Created by Ankui on 4/23/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T70-climbing-stairs.h"
---
+++ algorithm/algorithm/dynamic-programming/T70-climbing-stairs.h
@@ -0,0 +1,14 @@
+//
+//  T70-climbing-stairs.h
+//  algorithm
+//
+//  Created by Ankui on 4/23/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T70_climbing_stairs_h
+#define T70_climbing_stairs_h
+
+#include <stdio.h>
+
+#endif /* T70_climbing_stairs_h */
