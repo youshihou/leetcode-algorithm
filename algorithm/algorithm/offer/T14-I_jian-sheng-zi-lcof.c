705203bdb66e1266a29476b2578b5414fe7322db
oneday <youshihou5@gmail.com>
2021-04-24 18:01:25 +0800

    add T14-I_jian-sheng-zi-lcof

--- algorithm/algorithm/offer/T14-I_jian-sheng-zi-lcof.c
+++ algorithm/algorithm/offer/T14-I_jian-sheng-zi-lcof.c
@@ -6,4 +6,21 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/jian-sheng-zi-lcof/
+
+
 #include "T14-I_jian-sheng-zi-lcof.h"
+#include "algorithm-common.h"
+
+
+int cuttingRope(int n) {
+    int dp[n + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+        for (int j = 2; j < i; j++) {
+            dp[i] = fmax(dp[i], fmax(j * (i - j), j * dp[i - j]));
+        }
+    }
+    return dp[n];
+}
