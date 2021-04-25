45b0d49b51405e21f3cf819967871fe2cd7e8b35
oneday <youshihou5@gmail.com>
2021-04-25 11:45:34 +0800

    add T14-II_jian-sheng-zi-ii-lcof

---
+++ algorithm/algorithm/offer/T14-II_jian-sheng-zi-ii-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T14-II_jian-sheng-zi-ii-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 4/25/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T14-II_jian-sheng-zi-ii-lcof.h"
---
+++ algorithm/algorithm/offer/T14-II_jian-sheng-zi-ii-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T14-II_jian-sheng-zi-ii-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 4/25/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T14_II_jian_sheng_zi_ii_lcof_h
+#define T14_II_jian_sheng_zi_ii_lcof_h
+
+#include <stdio.h>
+
+#endif /* T14_II_jian_sheng_zi_ii_lcof_h */
