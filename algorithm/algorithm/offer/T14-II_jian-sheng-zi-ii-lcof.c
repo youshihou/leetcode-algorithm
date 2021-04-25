345e712b86346444585d96bc6f042a63f505cd61
oneday <youshihou5@gmail.com>
2021-04-25 11:45:44 +0800

    add T14-II_jian-sheng-zi-ii-lcof

--- algorithm/algorithm/offer/T14-II_jian-sheng-zi-ii-lcof.c
+++ algorithm/algorithm/offer/T14-II_jian-sheng-zi-ii-lcof.c
@@ -6,4 +6,18 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof/
+
+
 #include "T14-II_jian-sheng-zi-ii-lcof.h"
+#include "algorithm-common.h"
+
+int __cuttingRope(int n) {
+    if (n < 4) { return n - 1; }
+    long long result = 1;
+    while (n > 4) {
+        result = result * 3 % 1000000007;
+        n -= 3;
+    }
+    return (int)(result * n % 1000000007);
+}
