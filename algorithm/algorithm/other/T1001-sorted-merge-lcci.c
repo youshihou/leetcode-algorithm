f585fdf81f28efb251880eb96463c8db6b69ef3f
oneday <youshihou5@gmail.com>
2021-05-01 21:58:27 +0800

    add T1001-sorted-merge-lcci

---
+++ algorithm/algorithm/other/T1001-sorted-merge-lcci.c
@@ -0,0 +1,9 @@
+//
+//  T1001-sorted-merge-lcci.c
+//  algorithm
+//
+//  Created by Ankui on 5/1/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T1001-sorted-merge-lcci.h"
---
+++ algorithm/algorithm/other/T1001-sorted-merge-lcci.h
@@ -0,0 +1,14 @@
+//
+//  T1001-sorted-merge-lcci.h
+//  algorithm
+//
+//  Created by Ankui on 5/1/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T1001_sorted_merge_lcci_h
+#define T1001_sorted_merge_lcci_h
+
+#include <stdio.h>
+
+#endif /* T1001_sorted_merge_lcci_h */
