b289e22d9105b07595295669091f1e3006461a50
oneday <youshihou5@gmail.com>
2021-05-01 22:16:03 +0800

    add T136-single-number

--- algorithm/algorithm/other/T136-single-number.c
+++ algorithm/algorithm/other/T136-single-number.c
@@ -6,4 +6,24 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/single-number/
+
+
 #include "T136-single-number.h"
+#include "algorithm-common.h"
+
+int singleNumber(int* nums, int numsSize) {
+    for (int i = 1; i < numsSize; i++) {
+        nums[0] = nums[0] ^ nums[i];
+    }
+    return nums[0];
+}
+
+
+//int singleNumber(int* nums, int numsSize) {
+//    int result = 0;
+//    for (int i = 0; i < numsSize; i++) {
+//        result = result ^ nums[i];
+//    }
+//    return result;
+//}
