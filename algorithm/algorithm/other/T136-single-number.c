f2d4fcee44272f5c0b40a0921e0360e0e03ee4bb
oneday <youshihou5@gmail.com>
2021-05-01 22:15:31 +0800

    add T136-single-number

---
+++ algorithm/algorithm/other/T136-single-number.c
@@ -0,0 +1,9 @@
+//
+//  T136-single-number.c
+//  algorithm
+//
+//  Created by Ankui on 5/1/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T136-single-number.h"
---
+++ algorithm/algorithm/other/T136-single-number.h
@@ -0,0 +1,14 @@
+//
+//  T136-single-number.h
+//  algorithm
+//
+//  Created by Ankui on 5/1/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T136_single_number_h
+#define T136_single_number_h
+
+#include <stdio.h>
+
+#endif /* T136_single_number_h */
