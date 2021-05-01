dd6ff47db43d67f96500a480fe40c16d5b93bd67
oneday <youshihou5@gmail.com>
2021-05-01 21:58:39 +0800

    add T1001-sorted-merge-lcci

--- algorithm/algorithm/other/T1001-sorted-merge-lcci.c
+++ algorithm/algorithm/other/T1001-sorted-merge-lcci.c
@@ -6,4 +6,29 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/sorted-merge-lcci/
+
+
 #include "T1001-sorted-merge-lcci.h"
+#include "algorithm-common.h"
+
+
+void merge(int* A, int ASize, int m, int* B, int BSize, int n) {
+    int rA = m - 1;
+    int rB = n - 1;
+    int cur = m + n - 1;
+  
+    while (rA >= 0 || rB >= 0) {
+        if (rA == -1) {
+            A[cur] = B[rB--];
+        } else if (rB == -1) {
+            rA--;
+//            A[cur] = A[rA--];
+        } else if (A[rA] > B[rB]) {
+            A[cur] = A[rA--];
+        } else {
+            A[cur] = B[rB--];
+        }
+        cur--;
+    }
+}
