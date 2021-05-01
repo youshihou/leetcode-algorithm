a9b6d50c931a821044ef1bb985b7aa82756ca897
oneday <youshihou5@gmail.com>
2021-05-01 22:44:10 +0800

    add T268-missing-number

---
+++ algorithm/algorithm/other/T268-missing-number.h
@@ -0,0 +1,14 @@
+//
+//  T268-missing-number.h
+//  algorithm
+//
+//  Created by Ankui on 5/1/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T268_missing_number_h
+#define T268_missing_number_h
+
+#include <stdio.h>
+
+#endif /* T268_missing_number_h */
