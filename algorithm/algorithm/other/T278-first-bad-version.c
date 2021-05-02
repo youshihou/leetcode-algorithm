dd4854042af096f49caf387808378bf7e80a32df
oneday <youshihou5@gmail.com>
2021-05-02 22:40:52 +0800

    add T278-first-bad-version

---
+++ algorithm/algorithm/other/T278-first-bad-version.c
@@ -0,0 +1,9 @@
+//
+//  T278-first-bad-version.c
+//  algorithm
+//
+//  Created by Ankui on 5/2/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T278-first-bad-version.h"
---
+++ algorithm/algorithm/other/T278-first-bad-version.h
@@ -0,0 +1,14 @@
+//
+//  T278-first-bad-version.h
+//  algorithm
+//
+//  Created by Ankui on 5/2/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T278_first_bad_version_h
+#define T278_first_bad_version_h
+
+#include <stdio.h>
+
+#endif /* T278_first_bad_version_h */
