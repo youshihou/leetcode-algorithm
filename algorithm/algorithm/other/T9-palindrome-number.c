03cf805c633e9aea92a88eb18bdbc9759fdbdf32
oneday <youshihou5@gmail.com>
2021-05-02 23:11:08 +0800

    add T9-palindrome-number

---
+++ algorithm/algorithm/other/T9-palindrome-number.c
@@ -0,0 +1,9 @@
+//
+//  T9-palindrome-number.c
+//  algorithm
+//
+//  Created by Ankui on 5/2/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T9-palindrome-number.h"
---
+++ algorithm/algorithm/other/T9-palindrome-number.h
@@ -0,0 +1,14 @@
+//
+//  T9-palindrome-number.h
+//  algorithm
+//
+//  Created by Ankui on 5/2/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T9_palindrome_number_h
+#define T9_palindrome_number_h
+
+#include <stdio.h>
+
+#endif /* T9_palindrome_number_h */
