b0f6a626755bdc54c0ecc172b538e1e124926edf
oneday <youshihou5@gmail.com>
2021-05-02 22:41:01 +0800

    add T278-first-bad-version

--- algorithm/algorithm/other/T278-first-bad-version.c
+++ algorithm/algorithm/other/T278-first-bad-version.c
@@ -6,4 +6,27 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/first-bad-version/
+
+
+
 #include "T278-first-bad-version.h"
+#include "algorithm-common.h"
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int l = 1;
+    int r = n;
+    int m = 0;
+    while (l < r) {
+        m = l + ((r - l) >> 1);
+//        if (isBadVersion(m)) {
+//            r = m;
+//        } else {
+//            l = m + 1;
+//        }
+    }
+    return l;
+}
