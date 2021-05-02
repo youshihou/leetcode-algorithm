415900fb9a244c2bba3693e20000ec4fd1c5d61c
oneday <youshihou5@gmail.com>
2021-05-02 23:11:18 +0800

    add T9-palindrome-number

--- algorithm/algorithm/other/T9-palindrome-number.c
+++ algorithm/algorithm/other/T9-palindrome-number.c
@@ -6,4 +6,20 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/palindrome-number/
+
+
 #include "T9-palindrome-number.h"
+#include "algorithm-common.h"
+
+
+bool isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) { return false; }
+    
+    int result = 0;
+    while (x > result) {
+        result = result * 10 + x % 10;
+        x /= 10;
+    }
+    return x == result || x == result / 10;
+}
