932107c0f08134bb70314cd958aefb529c2b1b43
oneday <youshihou5@gmail.com>
2021-05-03 21:30:57 +0800

    add T13-roman-to-integer

--- algorithm/algorithm/other/T13-roman-to-integer.c
+++ algorithm/algorithm/other/T13-roman-to-integer.c
@@ -6,4 +6,54 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/roman-to-integer/
+
+
 #include "T13-roman-to-integer.h"
+#include "algorithm-common.h"
+
+int valueWithKey(char key) {
+    int res = 1000;
+    switch (key) {
+        case 'I':
+            res = 1;
+            break;
+        case 'V':
+            res = 5;
+            break;
+        case 'X':
+            res = 10;
+            break;
+        case 'L':
+            res = 50;
+            break;
+        case 'C':
+            res = 100;
+            break;
+        case 'D':
+            res = 500;
+            break;
+//        case 'M':
+//            res = 1000;
+//            break;
+        default:
+            break;
+    }
+    return res;
+}
+
+int romanToInt(char * s) {
+    int len = (int)strlen(s);
+    int res = 0;
+    for (int i = 0; i < len - 1; i++) {
+        int first = valueWithKey(s[i]);
+        int second = valueWithKey(s[i + 1]);
+        if (first >= second) {
+            res += first;
+        } else {
+            res -= first;
+        }
+    }
+    res += valueWithKey(s[len - 1]);
+    return res;
+}
