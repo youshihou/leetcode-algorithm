6f87a6d881b66e528651fb6cee35d8abdcca889b
oneday <youshihou5@gmail.com>
2021-05-04 15:37:29 +0800

    add T171-excel-sheet-column-number

---
+++ algorithm/algorithm/other/T171-excel-sheet-column-number.c
@@ -0,0 +1,9 @@
+//
+//  T171-excel-sheet-column-number.c
+//  algorithm
+//
+//  Created by Ankui on 5/4/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T171-excel-sheet-column-number.h"
---
+++ algorithm/algorithm/other/T171-excel-sheet-column-number.h
@@ -0,0 +1,14 @@
+//
+//  T171-excel-sheet-column-number.h
+//  algorithm
+//
+//  Created by Ankui on 5/4/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T171_excel_sheet_column_number_h
+#define T171_excel_sheet_column_number_h
+
+#include <stdio.h>
+
+#endif /* T171_excel_sheet_column_number_h */
