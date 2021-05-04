d5f3177ce0212333d330dd58d5f1201411de612d
oneday <youshihou5@gmail.com>
2021-05-04 11:11:32 +0800

    add T14-longest-common-prefix

---
+++ algorithm/algorithm/other/T14-longest-common-prefix.c
@@ -0,0 +1,9 @@
+//
+//  T14-longest-common-prefix.c
+//  algorithm
+//
+//  Created by Ankui on 5/3/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T14-longest-common-prefix.h"
---
+++ algorithm/algorithm/other/T14-longest-common-prefix.h
@@ -0,0 +1,14 @@
+//
+//  T14-longest-common-prefix.h
+//  algorithm
+//
+//  Created by Ankui on 5/3/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T14_longest_common_prefix_h
+#define T14_longest_common_prefix_h
+
+#include <stdio.h>
+
+#endif /* T14_longest_common_prefix_h */
