efec01df1bb06a217262d4faed736f7970d74838
oneday <youshihou5@gmail.com>
2021-05-04 15:37:42 +0800

    add T171-excel-sheet-column-number

--- algorithm/algorithm/other/T171-excel-sheet-column-number.c
+++ algorithm/algorithm/other/T171-excel-sheet-column-number.c
@@ -6,4 +6,33 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/excel-sheet-column-number/
+
+
 #include "T171-excel-sheet-column-number.h"
+#include "algorithm-common.h"
+
+
+int titleToNumber(char * columnTitle) {
+    if (columnTitle == NULL) { return 0; }
+    
+    int res = 0;
+    for (int i = 0; i < strlen(columnTitle); i++) {
+        res = res * 26 + (columnTitle[i] - 'A' + 1);
+    }
+    return res;
+}
+
+
+
+
+int _titleToNumber(char * columnTitle) {
+    if (columnTitle == NULL) { return 0; }
+    
+    int res = 0;
+    for (int i = 0; columnTitle[i] != '\0'; i++) {
+        int num = columnTitle[i] - 'A' + 1;
+        res = res * 26 + num;
+    }
+    return res;
+}
