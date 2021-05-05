997e4b8cbbab56ef09c42439a0fa6f5cdd799899
oneday <youshihou5@gmail.com>
2021-05-05 16:09:06 +0800

    add T746-min-cost-climbing-stairs

---
+++ algorithm/algorithm/other/T746-min-cost-climbing-stairs.c
@@ -0,0 +1,9 @@
+//
+//  T746-min-cost-climbing-stairs.c
+//  algorithm
+//
+//  Created by Ankui on 5/5/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T746-min-cost-climbing-stairs.h"
---
+++ algorithm/algorithm/other/T746-min-cost-climbing-stairs.h
@@ -0,0 +1,14 @@
+//
+//  T746-min-cost-climbing-stairs.h
+//  algorithm
+//
+//  Created by Ankui on 5/5/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T746_min_cost_climbing_stairs_h
+#define T746_min_cost_climbing_stairs_h
+
+#include <stdio.h>
+
+#endif /* T746_min_cost_climbing_stairs_h */
