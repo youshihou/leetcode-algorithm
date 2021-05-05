f0bc7af317ef7661e64a361aa8bdf6c17c595d24
oneday <youshihou5@gmail.com>
2021-05-05 19:58:08 +0800

    add T1030-matrix-cells-in-distance-order

---
+++ algorithm/algorithm/other/T1030-matrix-cells-in-distance-order.c
@@ -0,0 +1,9 @@
+//
+//  T1030-matrix-cells-in-distance-order.c
+//  algorithm
+//
+//  Created by Ankui on 5/5/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T1030-matrix-cells-in-distance-order.h"
---
+++ algorithm/algorithm/other/T1030-matrix-cells-in-distance-order.h
@@ -0,0 +1,14 @@
+//
+//  T1030-matrix-cells-in-distance-order.h
+//  algorithm
+//
+//  Created by Ankui on 5/5/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T1030_matrix_cells_in_distance_order_h
+#define T1030_matrix_cells_in_distance_order_h
+
+#include <stdio.h>
+
+#endif /* T1030_matrix_cells_in_distance_order_h */
