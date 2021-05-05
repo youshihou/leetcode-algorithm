b59f888cd50cbfc185da1d10f58a7eac46d98803
oneday <youshihou5@gmail.com>
2021-05-05 15:18:28 +0800

    add T415-add-strings

---
+++ algorithm/algorithm/other/T415-add-strings.c
@@ -0,0 +1,9 @@
+//
+//  T415-add-strings.c
+//  algorithm
+//
+//  Created by Ankui on 5/5/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T415-add-strings.h"
---
+++ algorithm/algorithm/other/T415-add-strings.h
@@ -0,0 +1,14 @@
+//
+//  T415-add-strings.h
+//  algorithm
+//
+//  Created by Ankui on 5/5/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T415_add_strings_h
+#define T415_add_strings_h
+
+#include <stdio.h>
+
+#endif /* T415_add_strings_h */
