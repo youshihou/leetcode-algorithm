0a20edbb990308beac11da58b985d05155c54f7d
oneday <youshihou5@gmail.com>
2021-05-05 16:23:54 +0800

    add T1603-design-parking-system

---
+++ algorithm/algorithm/other/T1603-design-parking-system.c
@@ -0,0 +1,9 @@
+//
+//  T1603-design-parking-system.c
+//  algorithm
+//
+//  Created by Ankui on 5/5/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T1603-design-parking-system.h"
---
+++ algorithm/algorithm/other/T1603-design-parking-system.h
@@ -0,0 +1,14 @@
+//
+//  T1603-design-parking-system.h
+//  algorithm
+//
+//  Created by Ankui on 5/5/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T1603_design_parking_system_h
+#define T1603_design_parking_system_h
+
+#include <stdio.h>
+
+#endif /* T1603_design_parking_system_h */
