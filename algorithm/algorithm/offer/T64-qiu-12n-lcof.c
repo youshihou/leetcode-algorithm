90af795152ee3420d05cb5a2a8090ac5403c19e6
oneday <youshihou5@gmail.com>
2021-05-10 20:52:24 +0800

    T64-qiu-12n-lcof

---
+++ algorithm/algorithm/offer/T64-qiu-12n-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T64-qiu-12n-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 5/10/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T64-qiu-12n-lcof.h"
---
+++ algorithm/algorithm/offer/T64-qiu-12n-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T64-qiu-12n-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 5/10/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T64_qiu_12n_lcof_h
+#define T64_qiu_12n_lcof_h
+
+#include <stdio.h>
+
+#endif /* T64_qiu_12n_lcof_h */
