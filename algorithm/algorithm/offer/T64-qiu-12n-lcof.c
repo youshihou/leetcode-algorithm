355c80dddd2f717cc5ca2f55293bca1992291fe4
oneday <youshihou5@gmail.com>
2021-05-10 20:52:39 +0800

    update code

--- algorithm/algorithm/offer/T64-qiu-12n-lcof.c
+++ algorithm/algorithm/offer/T64-qiu-12n-lcof.c
@@ -6,4 +6,19 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/qiu-12n-lcof/
+
+
 #include "T64-qiu-12n-lcof.h"
+#include "algorithm-common.h"
+
+
+int sumNums(int n) {
+    n && (n += sumNums(n - 1));
+    return n;
+}
+
+int _sumNums(int n) {
+    if (n == 0) { return 0; }
+    return n + sumNums(n - 1);
+}
