30bd9b95077833f08235670391627d2f337d76bb
oneday <youshihou5@gmail.com>
2021-05-12 15:09:31 +0800

    add T56-II_shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof

---
+++ algorithm/algorithm/offer/T56-II_shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.c
@@ -0,0 +1,9 @@
+//
+//  T56-II_shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.c
+//  algorithm
+//
+//  Created by Ankui on 5/12/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#include "T56-II_shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.h"
---
+++ algorithm/algorithm/offer/T56-II_shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.h
@@ -0,0 +1,14 @@
+//
+//  T56-II_shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.h
+//  algorithm
+//
+//  Created by Ankui on 5/12/21.
+//  Copyright © 2021 Ankui. All rights reserved.
+//
+
+#ifndef T56_II_shu_zu_zhong_shu_zi_chu_xian_de_ci_shu_ii_lcof_h
+#define T56_II_shu_zu_zhong_shu_zi_chu_xian_de_ci_shu_ii_lcof_h
+
+#include <stdio.h>
+
+#endif /* T56_II_shu_zu_zhong_shu_zi_chu_xian_de_ci_shu_ii_lcof_h */
