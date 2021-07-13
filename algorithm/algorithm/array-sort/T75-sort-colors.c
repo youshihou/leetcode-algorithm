eeac597d0c092b50bc876220298a0446076a4879
oneday <youshihou5@gmail.com>
2021-07-13 22:12:30 +0800

    update T75-sort-colors

--- algorithm/algorithm/array-sort/T75-sort-colors.c
+++ algorithm/algorithm/array-sort/T75-sort-colors.c
@@ -10,6 +10,34 @@
 
 #include "T75-sort-colors.h"
 
+void t75_swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void t75_sortColors(int* nums, int numsSize){
+    int i = 0;
+    int l = 0;
+    int r = numsSize - 1;
+
+    while (i <= r) {
+        int n = nums[i];
+        if (n == 0) {
+            t75_swap(&nums[i++], &nums[l++]);
+        } else if (n == 1) {
+            i++;
+        } else {
+            t75_swap(&nums[i], &nums[r--]);
+        }
+    }
+}
+
+
+
+
+
+
 void t75swap(int* nums, int i, int j) {
     int temp = nums[i];
     nums[i] = nums[j];
