a4fc7910670beddcfa6d0b5160090de787a019ee
oneday <youshihou5@gmail.com>
2021-07-26 22:58:14 +0800

    add T912-sort-an-array

--- algorithm/algorithm/array-sort/T912-sort-an-array.swift
+++ algorithm/algorithm/array-sort/T912-sort-an-array.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/sort-an-array/
+
+
 import Foundation
+
+class Solution {
+    func sortArray(_ nums: [Int]) -> [Int] {
+        if (nums.count <= 1) { return nums }
+
+        var left: [Int] = []
+        var right: [Int] = []
+        // let pivot = nums[0] // ERROR!!!
+        let pivot = nums[nums.count - 1]
+        for i in 0..<nums.count - 1 {
+            if nums[i] < pivot {
+                left.append(nums[i])
+            } else {
+                right.append(nums[i])
+            }
+        }
+        return sortArray(left) + [pivot] + sortArray(right)
+    }
+}
