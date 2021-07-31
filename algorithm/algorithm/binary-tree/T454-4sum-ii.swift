8d032e984eb8d0e6084496b30a1950a0c8f4726c
oneday <youshihou5@gmail.com>
2021-08-01 00:08:45 +0800

    add T454-4sum-ii

--- algorithm/algorithm/binary-tree/T454-4sum-ii.swift
+++ algorithm/algorithm/binary-tree/T454-4sum-ii.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2021 Ankui. All rights reserved.
 //
 
+// https://leetcode-cn.com/problems/4sum-ii/
+
+
 import Foundation
+
+func fourSumCount(_ nums1: [Int], _ nums2: [Int], _ nums3: [Int], _ nums4: [Int]) -> Int {
+    var count = 0
+    var map: [Int: Int] = [:]
+    for i in nums1 {
+        for j in nums2 {
+            map[i + j] = (map[i + j] ?? 0) + 1
+        }
+    }
+    for i in nums3 {
+        for j in nums4 {
+            count += map[0 - i - j] ?? 0
+        }
+    }
+    
+    return count
+}
